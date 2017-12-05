a8ea3c5e8871e58f28f8dfdea96570ab6208e032
vauxhall <zero.griffin@gmail.com>
2017-12-04 19:23:55 -0600

    Refactor view protocol

--- Source/View/Global/View/ViewTransitionableProtocol.swift
+++ Source/View/Global/View/ViewTransitionableProtocol.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-protocol ViewProtocol:class
+protocol ViewTransitionableProtocol:Any
 {
     weak var layoutLeft:NSLayoutConstraint! { get set }
     weak var layoutRight:NSLayoutConstraint! { get set }
     weak var layoutTop:NSLayoutConstraint! { get set }
     weak var layoutBottom:NSLayoutConstraint! { get set }
     weak var pushBackground:ViewPushBackground? { get set }
-    var panBack:Bool { get }
+    var shouldPanBack:Bool { get }
 }
