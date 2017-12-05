4280d72440f757d4340f7a5428137d2adc241feb
vauxhall <zero.griffin@gmail.com>
2017-12-05 16:24:16 -0600

    Update menu item new

--- Source/Model/Menu/MenuItemNew.swift
+++ Source/Model/Menu/MenuItemNew.swift
@@ -3,7 +3,6 @@ import UIKit
 struct MenuItemNew:MenuItemGenericProtocol
 {
     typealias A = ArchNew
-    
-    let order:Menu.Order = Menu.Order.n
-    let icon:UIImage = #imageLiteral(resourceName: "assetMenuHome")
+    let order:Menu.Order = Menu.Order.new
+    let icon:UIImage = #imageLiteral(resourceName: "assetMenuNew")
 }
