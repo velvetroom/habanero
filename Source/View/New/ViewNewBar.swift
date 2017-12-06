2dfe425c64b64fd84e49692d19166aae39c0e720
vauxhall <zero.griffin@gmail.com>
2017-12-06 17:27:33 -0600

    Add button add to bar

--- Source/View/New/ViewNewBar.swift
+++ Source/View/New/ViewNewBar.swift
@@ -6,11 +6,43 @@ final class ViewNewBar:ViewBar<ArchNew>
     {
         super.init(controller:controller)
         
-        self.labelTitle.text = String.localizedView(key:"ViewNewBar_labelTitle")
+        factoryBarViews()
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryBarViews()
+    {
+        let buttonAdd:UIButton = UIButton()
+        buttonAdd.translatesAutoresizingMaskIntoConstraints = false
+        buttonAdd.setTitleColor(
+            UIColor.colourSuccess,
+            for:UIControlState.normal)
+        buttonAdd.setTitleColor(
+            UIColor.colourGradientDark,
+            for:UIControlState.highlighted)
+        buttonAdd.setTitle(
+            String.localizedView(key:"ViewNewBar_buttonAdd"),
+            for:UIControlState())
+        
+        addSubview(buttonAdd)
+        
+        self.labelTitle.text = String.localizedView(key:"ViewNewBar_labelTitle")
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonAdd,
+            toView:self,
+            constant:ViewGlobal.Constants.contentTop)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonAdd,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonAdd,
+            constant:0)
     }
 }
