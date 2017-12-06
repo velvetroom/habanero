5a48f0341847a084a86ba18d8cf5c1cc532cd305
vauxhall <zero.griffin@gmail.com>
2017-12-05 19:47:54 -0600

    Create new header constants extension

---
+++ Source/View/New/ViewNewListHeader+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension ViewNewListHeader
+{
+    struct Constants
+    {
+        static let titleHeight:CGFloat = 40
+        static let titleLeft:CGFloat = 20
+        static let titleFontSize:CGFloat = 14
+    }
+}
