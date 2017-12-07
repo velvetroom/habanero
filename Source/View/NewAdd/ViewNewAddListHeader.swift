4692f30280ecf3f063b26be28601f0ce066c437f
vauxhall <zero.griffin@gmail.com>
2017-12-06 22:51:50 -0600

    Layout label title in header

--- Source/View/NewAdd/ViewNewAddListHeader.swift
+++ Source/View/NewAdd/ViewNewAddListHeader.swift
@@ -2,5 +2,48 @@ import UIKit
 
 final class ViewNewAddListHeader:UICollectionReusableView
 {
+    private weak var labelTitle:UILabel!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        self.clipsToBounds = true
+        self.backgroundColor = UIColor.clear
+        self.isUserInteractionEnabled = false
+        
+        self.factoryViews()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews()
+    {
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.textColor = UIColor(white:0, alpha:0.5)
+        labelTitle.font = UIFont.regular(size:ViewGlobal.Constants.barTitleFontSize)
+        self.labelTitle = labelTitle
+        
+        self.addSubview(labelTitle)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:ViewNewAddListHeader.Constants.titleHeight)
+        NSLayoutConstraint.leftToLeft(
+            view:labelTitle,
+            toView:self,
+            constant:ViewNewAddListHeader.Constants.titleLeft)
+        NSLayoutConstraint.widthGreaterOrEqual(
+            view:labelTitle)
+    }
 }
