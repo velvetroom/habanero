f6be1b2d7d87b4ef3a3bce74da3d7d34a8886cd3
vauxhall <zero.griffin@gmail.com>
2017-12-06 22:18:38 -0600

    Add view bar to newAdd

--- Source/View/NewAdd/ViewNewAdd.swift
+++ Source/View/NewAdd/ViewNewAdd.swift
@@ -2,5 +2,35 @@ import UIKit
 
 final class ViewNewAdd:ViewMain<ArchNewAdd>
 {
+    required init(controller:ControllerNewAdd)
+    {
+        super.init(controller:controller)
+        self.backgroundColor = UIColor.colourBackgroundGray
+        
+        self.factoryViews()
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews()
+    {
+        let viewBar:ViewNewAddBar = ViewNewAddBar(controller:controller)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:ViewGlobal.Constants.barHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
