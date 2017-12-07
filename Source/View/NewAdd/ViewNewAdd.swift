55ebf67be62a237a79c016eec37b0ed13d59483e
vauxhall <zero.griffin@gmail.com>
2017-12-06 22:20:51 -0600

    Allow pan back on newAdd

--- Source/View/NewAdd/ViewNewAdd.swift
+++ Source/View/NewAdd/ViewNewAdd.swift
@@ -2,6 +2,14 @@ import UIKit
 
 final class ViewNewAdd:ViewMain<ArchNewAdd>
 {
+    override var shouldPanBack:Bool
+    {
+        get
+        {
+            return true
+        }
+    }
+    
     required init(controller:ControllerNewAdd)
     {
         super.init(controller:controller)
