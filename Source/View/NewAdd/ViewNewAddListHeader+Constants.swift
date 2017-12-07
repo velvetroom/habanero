59149ec6bbdc8a18b442281bda74b7fcccbe59a8
vauxhall <zero.griffin@gmail.com>
2017-12-06 22:51:43 -0600

    Add header constants extension

---
+++ Source/View/NewAdd/ViewNewAddListHeader+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension ViewNewAddListHeader
+{
+    struct Constants
+    {
+        static let titleFontSize:CGFloat = 14
+        static let titleLeft:CGFloat = 20
+        static let titleHeight:CGFloat = 44
+    }
+}
