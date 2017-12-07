ee6f1e692c4d7a03d667d7f1a4e841bb140de7c7
vauxhall <zero.griffin@gmail.com>
2017-12-07 15:34:29 -0600

    Create new add constants extension

---
+++ Source/Model/NewAdd/NewAdd+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension NewAdd
+{
+    struct Constants
+    {
+        static let headerInfoHeight:CGFloat = 120
+        static let headerIngredientsHeight:CGFloat = 50
+        static let headerItemsHeight:CGFloat = 50
+    }
+}
