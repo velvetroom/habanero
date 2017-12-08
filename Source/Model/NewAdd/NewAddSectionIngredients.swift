e8f4bd3e6c6e13e24cba5c984bbdb05c33241d20
vauxhall <zero.griffin@gmail.com>
2017-12-07 22:02:31 -0600

    Create section ingredients struct

---
+++ Source/Model/NewAdd/NewAddSectionIngredients.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+struct NewAddSectionIngredients:NewAddSectionProtocol
+{
+    let title:String
+    let footerReusableIdentifier:String
+    let headerHeight:CGFloat
+    let items:[NewAddSectionItemProtocol]
+    
+    init()
+    {
+        self.title = String.localizedModel(key:"NewAdd_ingredientsSectionTitle")
+        self.footerReusableIdentifier = ViewNewAddListFooterIngredients.reusableIdentifier
+        self.headerHeight = NewAdd.Constants.headerIngredientsHeight
+        self.items = []
+    }
+}
