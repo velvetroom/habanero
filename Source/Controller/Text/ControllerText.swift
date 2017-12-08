b9d2ecaffedb2c961e1b71cf4dc4f39a2dbe044a
vauxhall <zero.griffin@gmail.com>
2017-12-08 01:10:00 -0600

    Become first responder on view did appear

--- Source/Controller/Text/ControllerText.swift
+++ Source/Controller/Text/ControllerText.swift
@@ -6,12 +6,30 @@ final class ControllerText:Controller<ArchText>
         text:String,
         completion:@escaping((String) -> ()))
     {
-        
         super.init()
+        
+        self.model.text = text
+        self.model.completion = completion
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override func viewDidAppear(_ animated:Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        guard
+        
+            let view:ViewText = self.view as? ViewText
+        
+        else
+        {
+            return
+        }
+        
+        view.viewInput.becomeFirstResponder()
     }
 }
