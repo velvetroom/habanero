27eb0c322d4aaf35b8cfd25feedad18bd7aab84e
vauxhall <zero.griffin@gmail.com>
2017-12-07 23:39:36 -0600

    Disable cell selection

--- Source/View/NewAdd/ViewNewAddList.swift
+++ Source/View/NewAdd/ViewNewAddList.swift
@@ -95,4 +95,18 @@ final class ViewNewAddList:ViewCollection<ArchNewAdd, ViewNewAddListCell, ViewCo
         
         return cell
     }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        shouldSelectItemAt indexPath:IndexPath) -> Bool
+    {
+        return false
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        shouldHighlightItemAt indexPath:IndexPath) -> Bool
+    {
+        return false
+    }
 }
