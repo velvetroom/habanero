4a22e74ead62b19f745bb006447d91b14ef2a2bc
vauxhall <zero.griffin@gmail.com>
2017-12-08 12:28:41 -0600

    Update controller with type alias

--- Source/View/Global/View/View.swift
+++ Source/View/Global/View/View.swift
@@ -3,9 +3,9 @@ import UIKit
 class View<A>:UIView, ViewControllableProtocol where A.C:Controller<A>
 {
     typealias C = A.C
-    private(set) weak var controller:A.C!
+    private(set) weak var controller:C!
     
-    required init(controller:A.C)
+    required init(controller:C)
     {
         super.init(frame:CGRect.zero)
         self.clipsToBounds = true
