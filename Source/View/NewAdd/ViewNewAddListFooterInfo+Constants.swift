a14f36d99ccf2074d7f9682c3ae80917c1c4306c
vauxhall <zero.griffin@gmail.com>
2017-12-07 22:25:52 -0600

    Create footer info constants struct

---
+++ Source/View/NewAdd/ViewNewAddListFooterInfo+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddListFooterInfo
+{
+    struct Constants
+    {
+        static let buttonFontSize:CGFloat = 14
+        static let buttonWidth:CGFloat = 80
+    }
+}
