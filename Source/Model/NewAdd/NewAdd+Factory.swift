22137581b767a836bfc2eb3fd5abaad78393ecbf
vauxhall <zero.griffin@gmail.com>
2017-12-07 22:04:08 -0600

    Update sections factory

--- Source/Model/NewAdd/NewAdd+Factory.swift
+++ Source/Model/NewAdd/NewAdd+Factory.swift
@@ -2,51 +2,13 @@ import Foundation
 
 extension NewAdd
 {
-    //MARK: private
-    
-    private class func factoryInfoSection() -> NewAddSection
-    {
-        let items:[NewAddSectionItemProtocol] = []
-        
-        let section:NewAddSection = NewAddSection(
-            title:String.localizedModel(key:"NewAdd_infoSectionTitle"),
-            headerHeight:NewAdd.Constants.headerInfoHeight,
-            items:items)
-        
-        return section
-    }
-    
-    private class func factoryIngredientsSection() -> NewAddSection
-    {
-        let items:[NewAddSectionItemProtocol] = []
-        
-        let section:NewAddSection = NewAddSection(
-            title:String.localizedModel(key:"NewAdd_ingredientsSectionTitle"),
-            headerHeight:NewAdd.Constants.headerIngredientsHeight,
-            items:items)
-        
-        return section
-    }
-    
-    private class func factoryStepsSection() -> NewAddSection
-    {
-        let items:[NewAddSectionItemProtocol] = []
-        
-        let section:NewAddSection = NewAddSection(
-            title:String.localizedModel(key:"NewAdd_stepsSectionTitle"),
-            headerHeight:NewAdd.Constants.headerStepsHeight,
-            items:items)
-        
-        return section
-    }
-    
     //MARK: internal
     
     class func factorySections() -> [NewAddSectionProtocol]
     {
         let infoSection:NewAddSectionProtocol = NewAddSectionInfo()
-        let ingredientsSection:NewAddSectionProtocol = NewAdd.factoryIngredientsSection()
-        let stepsSection:NewAddSectionProtocol = NewAdd.factoryStepsSection()
+        let ingredientsSection:NewAddSectionProtocol = NewAddSectionIngredients()
+        let stepsSection:NewAddSectionProtocol = NewAddSectionSteps()
         
         let sections:[NewAddSectionProtocol] = [
             infoSection,
