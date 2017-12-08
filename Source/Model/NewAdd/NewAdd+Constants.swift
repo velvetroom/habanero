7003a052a227ccf332238d780477deb5bd897f00
vauxhall <zero.griffin@gmail.com>
2017-12-07 22:59:37 -0600

    Increase headers height

--- Source/Model/NewAdd/NewAdd+Constants.swift
+++ Source/Model/NewAdd/NewAdd+Constants.swift
@@ -5,7 +5,7 @@ extension NewAdd
     struct Constants
     {
         static let headerInfoHeight:CGFloat = 120
-        static let headerIngredientsHeight:CGFloat = 50
-        static let headerStepsHeight:CGFloat = 50
+        static let headerIngredientsHeight:CGFloat = 60
+        static let headerStepsHeight:CGFloat = 60
     }
 }
