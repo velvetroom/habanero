c7fe513d27ec1382f4dc2e701fc2f5d5c1326a77
vauxhall <zero.griffin@gmail.com>
2017-12-12 13:22:56 -0600

    Create footer constants

---
+++ Source/View/NewAddIngredient/ViewNewAddIngredientListFooter+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddIngredientListFooter
+{
+    struct Constants
+    {
+        static let buttonHeight:CGFloat = 40
+        static let buttonFontSize:CGFloat = 14
+    }
+}
