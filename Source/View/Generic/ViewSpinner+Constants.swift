766adb734f2f257c9e88ce1a21991645d4d841a3
vauxhall <zero.griffin@gmail.com>
2017-12-12 01:48:21 -0600

    Update spinner view

--- Source/View/Generic/ViewSpinner+Constants.swift
+++ Source/View/Generic/ViewSpinner+Constants.swift
@@ -5,8 +5,17 @@ extension ViewSpinner
     struct Constants
     {
         static let images:[UIImage] = [
-            ]
+            #imageLiteral(resourceName: "assetSpinner00"),
+            #imageLiteral(resourceName: "assetSpinner01"),
+            #imageLiteral(resourceName: "assetSpinner02"),
+            #imageLiteral(resourceName: "assetSpinner03"),
+            #imageLiteral(resourceName: "assetSpinner04"),
+            #imageLiteral(resourceName: "assetSpinner05"),
+            #imageLiteral(resourceName: "assetSpinner06"),
+            #imageLiteral(resourceName: "assetSpinner07"),
+            #imageLiteral(resourceName: "assetSpinner08"),
+            #imageLiteral(resourceName: "assetSpinner09")]
         
-        static let animationDuration:TimeInterval = 1
+        static let animationDuration:TimeInterval = 3
     }
 }
