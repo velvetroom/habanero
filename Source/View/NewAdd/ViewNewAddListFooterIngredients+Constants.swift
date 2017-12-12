7fb97647b3b2f91091dce1c5816b0c0435104487
vauxhall <zero.griffin@gmail.com>
2017-12-12 00:38:11 -0600

    Create constants extension

---
+++ Source/View/NewAdd/ViewNewAddListFooterIngredients+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddListFooterIngredients
+{
+    struct Constants
+    {
+        static let buttonFontSize:CGFloat = 14
+        static let buttonWidth:CGFloat = 120
+    }
+}
