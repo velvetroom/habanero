4a33c6b883c062a85a4e335b6fb179c8a4b471f7
vauxhall <zero.griffin@gmail.com>
2017-12-12 00:36:06 -0600

    Layout view bar

--- Source/View/NewAddIngredient/ViewNewAddIngredient.swift
+++ Source/View/NewAddIngredient/ViewNewAddIngredient.swift
@@ -2,5 +2,25 @@ import UIKit
 
 final class ViewNewAddIngredient:ViewMain<ArchNewAddIngredient>
 {
+    private(set) weak var viewBar:ViewNewAddIngredientBar!
     
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let viewBar:ViewNewAddIngredientBar = ViewNewAddIngredientBar(controller:self.controller)
+        self.viewBar = viewBar
+        
+        self.addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:ViewNewAddIngredient.Constants.barHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
