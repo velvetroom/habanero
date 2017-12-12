6068d23e75caf3be152310eed40c834d0d528066
vauxhall <zero.griffin@gmail.com>
2017-12-12 01:45:13 -0600

    Add spinner assets

---
+++ Source/Controller/New/ControllerNewAdd+Ingredients.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+extension ControllerNewAdd
+{
+    //MARK: internal
+    
+    func transitionAddIngredient()
+    {
+        guard
+        
+            let build:Build = self.model.build,
+            let database:Database = self.model.database
+        
+        else
+        {
+            return
+        }
+        
+        
+    }
+}
--- Source/Controller/New/ControllerNewAddIngredient.swift
+++ Source/Controller/New/ControllerNewAddIngredient.swift
@@ -2,5 +2,15 @@ import Foundation
 
 final class ControllerNewAddIngredient:Controller<ArchNewAddIngredient>
 {
+    init(
+        build:Build,
+        database:Database)
+    {
+        super.init()
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
