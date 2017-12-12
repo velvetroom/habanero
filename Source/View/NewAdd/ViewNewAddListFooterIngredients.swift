8c1ad2a5405347fd1ce5fe94db5d466b94f4a1ce
vauxhall <zero.griffin@gmail.com>
2017-12-12 00:44:56 -0600

    Layout button add ingredient

--- Source/View/NewAdd/ViewNewAddListFooterIngredients.swift
+++ Source/View/NewAdd/ViewNewAddListFooterIngredients.swift
@@ -2,5 +2,32 @@ import UIKit
 
 final class ViewNewAddListFooterIngredients:ViewNewAddListFooter
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let buttonAdd:UIButton = UIButton()
+        buttonAdd.setTitleColor(
+            UIColor.colourSuccess,
+            for:UIControlState.normal)
+        buttonAdd.setTitleColor(
+            UIColor(white:0, alpha:0.2),
+            for:UIControlState.highlighted)
+        buttonAdd.setTitle(
+            String.localizedView(key:"ViewNewAddListFooterIngredients_buttonAdd"),
+            for:UIControlState())
+        buttonAdd.titleLabel!.font = UIFont.bold(
+            size:ViewNewAddListFooterIngredients.Constants.buttonFontSize)
+        self.addSubview(buttonAdd)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:buttonAdd,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonAdd,
+            constant:ViewNewAddListFooterIngredients.Constants.buttonWidth)
+        NSLayoutConstraint.rightToRight(
+            view:buttonAdd,
+            toView:self)
+    }
 }
