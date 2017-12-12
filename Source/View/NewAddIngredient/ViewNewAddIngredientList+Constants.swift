e2dff1d56aea4a4d60c907891726d714b8833199
vauxhall <zero.griffin@gmail.com>
2017-12-12 13:12:35 -0600

    Create constants extension for list

---
+++ Source/View/NewAddIngredient/ViewNewAddIngredientList+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddIngredientList
+{
+    struct Constants
+    {
+        static let headerHeight:CGFloat = 150
+        static let footerHeight:CGFloat = 70
+    }
+}
--- Source/View/NewAddIngredient/ViewNewAddIngredientListHeader.swift
+++ Source/View/NewAddIngredient/ViewNewAddIngredientListHeader.swift
@@ -2,5 +2,9 @@ import UIKit
 
 final class ViewNewAddIngredientListHeader:ViewCollectionReusable
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        isUserInteractionEnabled = false
+    }
 }
