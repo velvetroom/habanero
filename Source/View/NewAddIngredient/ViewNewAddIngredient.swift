e538dafcc38b7433384948cf14a6ec1d78a1afd4
vauxhall <zero.griffin@gmail.com>
2017-12-12 02:02:34 -0600

    Add spinner to ingredients view

--- Source/View/NewAddIngredient/ViewNewAddIngredient.swift
+++ Source/View/NewAddIngredient/ViewNewAddIngredient.swift
@@ -3,15 +3,29 @@ import UIKit
 final class ViewNewAddIngredient:ViewMain<ArchNewAddIngredient>
 {
     private(set) weak var viewBar:ViewNewAddIngredientBar!
+    private weak var viewSpinner:ViewSpinner?
+    
+    deinit
+    {
+        self.viewSpinner?.stopAnimating()
+    }
     
     override func factoryViews()
     {
         super.factoryViews()
         
+        let viewSpinner:ViewSpinner = ViewSpinner()
+        self.viewSpinner = viewSpinner
+        
         let viewBar:ViewNewAddIngredientBar = ViewNewAddIngredientBar(controller:self.controller)
         self.viewBar = viewBar
         
+        self.addSubview(viewSpinner)
         self.addSubview(viewBar)
+        
+        NSLayoutConstraint.equals(
+            view:viewSpinner,
+            toView:self)
         
         NSLayoutConstraint.topToTop(
             view:viewBar,
