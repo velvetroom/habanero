175151482a85cfc11792239f30bb67739b7ae860
vauxhall <zero.griffin@gmail.com>
2017-12-12 13:21:31 -0600

    Add header title

---
+++ Source/View/NewAddIngredient/ViewNewAddIngredientListHeader+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension ViewNewAddIngredientListHeader
+{
+    struct Constants
+    {
+        static let fontSize:CGFloat = 14
+        static let titleHeight:CGFloat = 50
+        static let titleMarginHorizontal:CGFloat = 20
+    }
+}
--- Source/View/NewAddIngredient/ViewNewAddIngredientListHeader.swift
+++ Source/View/NewAddIngredient/ViewNewAddIngredientListHeader.swift
@@ -5,6 +5,27 @@ final class ViewNewAddIngredientListHeader:ViewCollectionReusable
     override func factoryViews()
     {
         super.factoryViews()
-        isUserInteractionEnabled = false
+        self.isUserInteractionEnabled = false
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.numberOfLines = 0
+        labelTitle.textAlignment = NSTextAlignment.center
+        labelTitle.font = UIFont.regular(size:ViewNewAddIngredientListHeader.Constants.fontSize)
+        labelTitle.text = String.localizedView(key:"ViewNewAddIngredientListHeader_labelTitle")
+        self.addSubview(labelTitle)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:ViewNewAddIngredientListHeader.Constants.titleHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelTitle,
+            toView:self,
+            margin:ViewNewAddIngredientListHeader.Constants.titleMarginHorizontal)
     }
 }
