fbabe50e9e4e4042981f871c7f167b5eb7ffa136
vauxhall <zero.griffin@gmail.com>
2017-12-12 00:30:51 -0600

    Add search bar

--- Source/View/NewAddIngredient/ViewNewAddIngredientBar.swift
+++ Source/View/NewAddIngredient/ViewNewAddIngredientBar.swift
@@ -2,5 +2,35 @@ import UIKit
 
 final class ViewNewAddIngredientBar:ViewBarBack<ArchNewAddIngredient>
 {
+    private(set) weak var searchBar:UISearchBar!
     
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let searchBar:UISearchBar = UISearchBar(frame:CGRect.zero)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.backgroundColor = UIColor.clear
+        searchBar.searchBarStyle = UISearchBarStyle.minimal
+        searchBar.showsCancelButton = true
+        searchBar.tintColor = UIColor.colourBackgroundDark
+        searchBar.delegate = self
+        self.searchBar = searchBar
+        
+        addSubview(searchBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:searchBar,
+            toView:self,
+            constant:VCreateSearchBaseBar.Constants.barTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:searchBar,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:searchBar,
+            toView:buttonBack)
+        NSLayoutConstraint.rightToRight(
+            view:searchBar,
+            toView:self)
+    }
 }
