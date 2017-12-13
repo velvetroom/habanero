5fb58ee14a37910d145115f8d1fe5d17a781e64e
vauxhall <zero.griffin@gmail.com>
2017-12-13 17:41:41 -0600

    Create constants extension

---
+++ Source/View/NewAdd/ViewNewAddListCellIngredient+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddListCellIngredient
+{
+    struct Constants
+    {
+        static let fontSize:CGFloat = 16
+        static let buttonWidth:CGFloat = 60
+    }
+}
