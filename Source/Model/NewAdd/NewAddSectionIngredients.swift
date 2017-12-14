821d9191d0d92d7f9d53f8730a2eb5e815096b73
vauxhall <zero.griffin@gmail.com>
2017-12-13 20:05:29 -0600

    Factory ingredient items

--- Source/Model/NewAdd/NewAddSectionIngredients.swift
+++ Source/Model/NewAdd/NewAddSectionIngredients.swift
@@ -12,6 +12,43 @@ struct NewAddSectionIngredients:NewAddSectionProtocol
         self.title = String.localizedModel(key:"NewAdd_ingredientsSectionTitle")
         self.footerReusableIdentifier = ViewNewAddListFooterIngredients.reusableIdentifier
         self.headerHeight = NewAdd.Constants.headerIngredientsHeight
-        self.items = []
+        self.items = NewAddSectionIngredients.factoryItems(build:build)
+    }
+    
+    //MARK: private
+    
+    private static func factoryItems(build:Build) -> [NewAddSectionItemIngredient]
+    {
+        guard
+            
+            let ingredients:[BuildIngredient] = build.ingredientList
+        
+        else
+        {
+            return []
+        }
+        
+        var items:[NewAddSectionItemIngredient] = []
+        
+        for ingredient:BuildIngredient in ingredients
+        {
+            guard
+            
+                let name:String = ingredient.name,
+                let amount:String = ViewFormat.factoryAmount(ingredient:ingredient)
+            
+            else
+            {
+                continue
+            }
+            
+            let item:NewAddSectionItemIngredient = NewAddSectionItemIngredient(
+                name:name,
+                amount:amount)
+            
+            items.append(item)
+        }
+        
+        return items
     }
 }
