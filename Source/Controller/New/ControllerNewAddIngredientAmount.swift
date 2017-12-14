3013121af1101f5e9db441f6711417e883e2da69
vauxhall <zero.griffin@gmail.com>
2017-12-13 20:43:42 -0600

    Instance ingredient amount controller

--- Source/Controller/New/ControllerNewAddIngredientAmount.swift
+++ Source/Controller/New/ControllerNewAddIngredientAmount.swift
@@ -2,5 +2,20 @@ import Foundation
 
 final class ControllerNewAddIngredientAmount:Controller<ArchNewAddIngredientAmount>
 {
+    init(
+        ingredient:Ingredient,
+        build:Build,
+        database:Database)
+    {
+        super.init()
+        
+        self.model.ingredient = ingredient
+        self.model.build = build
+        self.model.database = database
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/Model/NewAddIngredientAmount/NewAddIngredientAmount.swift
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmount.swift
@@ -2,5 +2,7 @@ import Foundation
 
 final class NewAddIngredientAmount:Model<ArchNewAddIngredientAmount>
 {
-    
+    var ingredient:Ingredient?
+    var build:Build?
+    var database:Database?
 }
