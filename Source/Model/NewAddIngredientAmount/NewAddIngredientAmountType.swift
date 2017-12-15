24a72a16a88a7eecefb315706de07e5f63a49cc1
vauxhall <zero.griffin@gmail.com>
2017-12-15 15:59:57 -0600

    Create ingredient quantity

--- Source/Model/NewAddIngredientAmount/NewAddIngredientAmountType.swift
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmountType.swift
@@ -5,9 +5,9 @@ struct NewAddIngredientAmountType
     struct Constants
     {
         static let defaultCups:Float = 1
+        static let defaultQuantity:Float = 1
         static let defaultGrams:Int32 = 1
         static let defaultMililitres:Int32 = 1
-        static let defaultQuantity:Float = 1
         static let defaultSpoons:Float = 1
     }
 }
--- Source/Model/NewAddIngredientAmount/NewAddIngredientAmountTypeQuantity.swift
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmountTypeQuantity.swift
@@ -4,11 +4,37 @@ struct NewAddIngredientAmountTypeQuantity:NewAddIngredientAmountTypeProtocol
 {
     let name:String = String.localizedModel(key:"NewAddIngredientAmountTypeQuantity_name")
     
+    //MARK: private
+    
+    private static func amountToQuantity(amount:String) -> Float
+    {
+        guard
+            
+            let quantity:Float = Float(amount)
+            
+        else
+        {
+            return NewAddIngredientAmountType.Constants.defaultQuantity
+        }
+        
+        return quantity
+    }
+    
+    //MARK: internal
+    
     func createIngredient(
         amount:String,
         database:Database,
         completion:@escaping((BuildIngredient) -> ()))
     {
+        let quantity:Float = NewAddIngredientAmountTypeQuantity.amountToQuantity(amount:amount)
         
+        database.create
+        { (ingredient:BuildIngredientQuantity) in
+            
+            ingredient.quantity = quantity
+            
+            completion(ingredient)
+        }
     }
 }
