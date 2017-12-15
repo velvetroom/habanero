593fec3a9560918510947d613647853007eff941
vauxhall <zero.griffin@gmail.com>
2017-12-15 15:51:52 -0600

    Create default struct

---
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmountType.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct NewAddIngredientAmountType
+{
+    struct Constants
+    {
+        static let defaultCups:Float = 1
+        static let defaultGrams:UInt32 = 1
+        static let defaultMililitres:Float = 1
+        static let defaultQuantity:Float = 1
+        static let defaultSpoons:Float = 1
+    }
+}
