438ead504a01f99ef363b83a132d57c51fc91dfd
vauxhall <zero.griffin@gmail.com>
2017-12-15 01:34:53 -0600

    Create actions extension

---
+++ Source/View/NewAddIngredientAmount/ViewNewAddIngredientAmountList+Actions.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension ViewNewAddIngredientAmountList
+{
+    //MARK: internal
+    
+    func modelAtIndex(index:IndexPath) -> NewAddIngredientAmountTypeProtocol
+    {
+        let item:NewAddIngredientAmountTypeProtocol = self.controller.model.types[index.item]
+        
+        return item
+    }
+}
