a8545aa5da11749fa17b0027ccba3f4b39adb84a
vauxhall <zero.griffin@gmail.com>
2017-12-15 15:54:23 -0600

    Create ingredient mililitres

--- Source/Model/NewAddIngredientAmount/NewAddIngredientAmountTypeMililitres.swift
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmountTypeMililitres.swift
@@ -4,11 +4,37 @@ struct NewAddIngredientAmountTypeMililitres:NewAddIngredientAmountTypeProtocol
 {
     let name:String = String.localizedModel(key:"NewAddIngredientAmountTypeMililitres_name")
     
+    //MARK: private
+    
+    private static func amountToMililitres(amount:String) -> Int32
+    {
+        guard
+            
+            let mililitres:Int32 = Int32(amount)
+            
+        else
+        {
+            return NewAddIngredientAmountType.Constants.defaultMililitres
+        }
+        
+        return mililitres
+    }
+    
+    //MARK: internal
+    
     func createIngredient(
         amount:String,
         database:Database,
         completion:@escaping((BuildIngredient) -> ()))
     {
+        let mililitres:Int32 = NewAddIngredientAmountTypeMililitres.amountToMililitres(amount:amount)
         
+        database.create
+        { (ingredient:BuildIngredientMililitres) in
+            
+            ingredient.mililitres = mililitres
+            
+            completion(ingredient)
+        }
     }
 }
