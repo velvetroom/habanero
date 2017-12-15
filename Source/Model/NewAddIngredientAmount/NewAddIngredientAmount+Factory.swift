ada3f8873fd7b3c98e1ca48a49a6a3f3f6fdf82d
vauxhall <zero.griffin@gmail.com>
2017-12-15 01:26:44 -0600

    Factory types

--- Source/Model/NewAddIngredientAmount/NewAddIngredientAmount+Factory.swift
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmount+Factory.swift
@@ -2,5 +2,23 @@ import Foundation
 
 extension NewAddIngredientAmount
 {
+    //MARK: internal
     
+    class func factoryTypes() -> [NewAddIngredientAmountTypeProtocol]
+    {
+        let typeCups:NewAddIngredientAmountTypeCups = NewAddIngredientAmountTypeCups()
+        let typeGrams:NewAddIngredientAmountTypeGrams = NewAddIngredientAmountTypeGrams()
+        let typeMililitres:NewAddIngredientAmountTypeMililitres = NewAddIngredientAmountTypeMililitres()
+        let typeQuantity:NewAddIngredientAmountTypeQuantity = NewAddIngredientAmountTypeQuantity()
+        let typeSpoons:NewAddIngredientAmountTypeSpoons = NewAddIngredientAmountTypeSpoons()
+        
+        let types:[NewAddIngredientAmountTypeProtocol] = [
+            typeQuantity,
+            typeCups,
+            typeSpoons,
+            typeGrams,
+            typeMililitres]
+        
+        return types
+    }
 }
--- Source/Model/NewAddIngredientAmount/NewAddIngredientAmount.swift
+++ Source/Model/NewAddIngredientAmount/NewAddIngredientAmount.swift
@@ -5,4 +5,13 @@ final class NewAddIngredientAmount:Model<ArchNewAddIngredientAmount>
     var ingredient:Ingredient?
     var build:Build?
     var database:Database?
+    
+    let types:[NewAddIngredientAmountTypeProtocol]
+    
+    required init()
+    {
+        self.types = NewAddIngredientAmount.factoryTypes()
+        
+        super.init()
+    }
 }
