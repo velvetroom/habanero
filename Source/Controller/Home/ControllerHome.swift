e3c9934dbcf29d488d8cb2aaa4b94d296efcd8e9
vauxhall <zero.griffin@gmail.com>
2017-12-15 17:18:03 -0600

    Load settings on home

--- Source/Controller/Home/ControllerHome.swift
+++ Source/Controller/Home/ControllerHome.swift
@@ -2,5 +2,23 @@ import Foundation
 
 final class ControllerHome:Controller<ArchHome>
 {
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.view.superview?.isUserInteractionEnabled = false
+        
+        self.model.loadSettings
+        { [weak self] in
+        
+            self?.settingsLoaded()
+        }
+    }
     
+    //MARK: private
+    
+    private func settingsLoaded()
+    {
+        
+    }
 }
---
+++ Source/Model/Home/Home+Settings.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension Home
+{
+    //MARK: private
+    
+    private func asyncLoadSettings(completion:@escaping(() -> ()))
+    {
+        
+    }
+    
+    //MARK: internal
+    
+    func loadSettings(completion:@escaping(() -> ()))
+    {
+        
+    }
+}
