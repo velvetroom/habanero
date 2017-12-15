98ff013a1379a063dd303de971fa4051d96b2176
vauxhall <zero.griffin@gmail.com>
2017-12-14 22:19:17 -0600

    Create constants extensions

---
+++ Source/View/NewAddIngredientAmount/ViewNewAddIngredientAmountInput+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddIngredientAmountInput
+{
+    struct Constants
+    {
+        static let titleFontSize:CGFloat = 14
+        static let fieldFontSize:CGFloat = 20
+    }
+}
