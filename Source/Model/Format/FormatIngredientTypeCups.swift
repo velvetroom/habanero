b0741065e79f15ca1ea88ffcb3c7d7d5c6ec5210
vauxhall <zero.griffin@gmail.com>
2017-12-16 20:58:46 -0600

    Create format type cups

---
+++ Source/Model/Format/FormatIngredientTypeCups.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct FormatIngredientTypeCups:FormatIngredientTypeProtocol
+{
+    //MARK: internal
+    
+    static func factoryAmount(ingredient:BuildIngredient) -> String?
+    {
+        return nil
+    }
+}
