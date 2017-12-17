4c5672401b7cbd9000d61e8aa56939a720d76427
vauxhall <zero.griffin@gmail.com>
2017-12-16 21:01:43 -0600

    Create format spoons

---
+++ Source/Model/Format/FormatIngredientTypeSpoons.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct FormatIngredientTypeSpoons:FormatIngredientTypeProtocol
+{
+    //MARK: internal
+    
+    static func factoryAmount(buildIngredient:BuildIngredient) -> String?
+    {
+        return nil
+    }
+}
