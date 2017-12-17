c85b2fc8dfee42399ff157b33c518fac6d48391c
vauxhall <zero.griffin@gmail.com>
2017-12-17 17:14:15 -0600

    Factory grams metrics

--- Source/Model/Format/FormatIngredientTypeGrams.swift
+++ Source/Model/Format/FormatIngredientTypeGrams.swift
@@ -2,12 +2,100 @@ import Foundation
 
 struct FormatIngredientTypeGrams:FormatIngredientTypeProtocol
 {
+    private static var lowerMetricsRouter:[SettingsMetrics : ((Int) -> (String?))]
+    {
+        get
+        {
+            let map:[SettingsMetrics : ((Int) -> (String?))] = [
+                SettingsMetrics.international : FormatIngredientTypeGrams.factoryGrams,
+                SettingsMetrics.imperial : FormatIngredientTypeGrams.factoryOunces]
+            
+            return map
+        }
+    }
+    
+    private static var upperMetricsRouter:[SettingsMetrics : ((Int) -> (String?))]
+    {
+        get
+        {
+            let map:[SettingsMetrics : ((Int) -> (String?))] = [
+                SettingsMetrics.international : FormatIngredientTypeGrams.factoryKilograms,
+                SettingsMetrics.imperial : FormatIngredientTypeGrams.factoryPounds]
+            
+            return map
+        }
+    }
+    
+    //MARK: private
+    
+    private static func factoryRouter(
+        grams:Int,
+        metrics:SettingsMetrics) -> ((Int) -> (String?))?
+    {
+        let router:((Int) -> (String?))?
+        
+        if grams > FormatIngredientTypeGrams.Constants.thousand
+        {
+            router = FormatIngredientTypeGrams.upperMetricsRouter[metrics]
+        }
+        else
+        {
+            router = FormatIngredientTypeGrams.lowerMetricsRouter[metrics]
+        }
+        
+        return router
+    }
+    
+    private static func factoryKilograms(grams:Int) -> String?
+    {
+        
+    }
+    
+    private static func factoryGrams(grams:Int) -> String?
+    {
+        
+    }
+    
+    private static func factoryPounds(grams:Int) -> String?
+    {
+        
+    }
+    
+    private static func factoryOunces(grams:Int) -> String?
+    {
+        
+    }
+    
     //MARK: internal
     
     static func factoryAmount(
         buildIngredient:BuildIngredient,
         settings:Settings) -> String?
     {
-        return nil
+        guard
+            
+            let buildIngredient:BuildIngredientGrams = buildIngredient as? BuildIngredientGrams
+            
+        else
+        {
+            return nil
+        }
+        
+        let grams:Int = Int(buildIngredient.grams)
+        
+        guard
+            
+            let router:((Int) -> (String?)) = FormatIngredientTypeGrams.factoryRouter(
+                grams:grams,
+                metrics:settings.metrics)
+        
+        else
+        {
+            return nil
+        }
+        
+        let amount:String? = router(grams)
+        
+        return amount
     }
 }
