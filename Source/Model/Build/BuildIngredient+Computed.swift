a243e668270761b393b3b2f791f14ab091ce10e5
vauxhall <zero.griffin@gmail.com>
2017-12-16 21:11:57 -0600

    Add ingredient type computed property

--- Source/Model/Build/BuildIngredient+Computed.swift
+++ Source/Model/Build/BuildIngredient+Computed.swift
@@ -2,4 +2,20 @@ import Foundation
 
 extension BuildIngredient
 {
+    var ingredientType:IngredientType
+    {
+        get
+        {
+            guard
+            
+                let ingredientType:IngredientType = IngredientType(rawValue:self.rawIngredientType)
+            
+            else
+            {
+                return IngredientType.quantity
+            }
+            
+            return ingredientType
+        }
+    }
 }
