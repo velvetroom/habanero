da0a5f0da5b4fe8105664d0bb75a5bf1368428a3
vauxhall <zero.griffin@gmail.com>
2017-12-16 18:29:05 -0600

    Load or create settings

--- Source/Model/Home/Home+Settings.swift
+++ Source/Model/Home/Home+Settings.swift
@@ -6,7 +6,66 @@ extension Home
     
     private func asyncLoadSettings(completion:@escaping(() -> ()))
     {
+        guard
         
+            let database:Database = Database(bundle:Bundle.main)
+        
+        else
+        {
+            return
+        }
+        
+        self.database = database
+        
+        database.fetch
+        { [weak self] (settingsList:[Settings]) in
+            
+            guard
+            
+                let settings:Settings = settingsList.first
+            
+            else
+            {
+                self?.createSettings(
+                    database:database,
+                    completion:completion)
+                
+                return
+            }
+            
+            self?.settingsLoaded(
+                settings:settings,
+                completion:completion)
+        }
+    }
+    
+    private func createSettings(
+        database:Database,
+        completion:@escaping(() -> ()))
+    {
+        database.create
+        { (settings:Settings) in
+            
+            database.save
+            { [weak self] in
+                
+                self?.settingsLoaded(
+                    settings:settings,
+                    completion:completion)
+            }
+        }
+    }
+    
+    private func settingsLoaded(
+        settings:Settings,
+        completion:@escaping(() -> ()))
+    {
+        self.settings = settings
+        
+        DispatchQueue.main.async
+        {
+            completion()
+        }
     }
     
     //MARK: internal
