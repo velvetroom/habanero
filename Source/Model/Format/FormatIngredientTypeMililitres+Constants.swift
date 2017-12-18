8bb6b99b621b3326cea43745716816125812c433
vauxhall <zero.griffin@gmail.com>
2017-12-17 22:49:16 -0600

    Add mililitres constants

---
+++ Source/Model/Format/FormatIngredientTypeMililitres+Constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension FormatIngredientTypeMililitres
+{
+    struct Constants
+    {
+        static let thousand:Int = 1000
+        static let milisPerLitre:Float = 1000
+        static let milisPerGallon:Float = 3785.41
+        static let milisPerFluidOunce:Float = 29.5735
+        static let decimalsUpperMetrics:Int = 1
+        static let decimalsLowerMetrics:Int = 0
+    }
+}
