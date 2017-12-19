3e863f28b48d1f8a89fa1c95a6b15882695bfdb3
vauxhall <zero.griffin@gmail.com>
2017-12-19 00:12:35 -0600

    Factory step items

--- Source/Model/NewAdd/NewAddSectionSteps.swift
+++ Source/Model/NewAdd/NewAddSectionSteps.swift
@@ -2,6 +2,18 @@ import UIKit
 
 struct NewAddSectionSteps:NewAddSectionProtocol
 {
+    private static var stepRouters:[RecipeStepType:((BuildStep) -> (NewAddSectionItemProtocol?))]
+    {
+        get
+        {
+            let map:[RecipeStepType:((BuildStep) -> (NewAddSectionItemProtocol?))] = [
+                RecipeStepType.image : NewAddSectionSteps.factoryStepImage,
+                RecipeStepType.text : NewAddSectionSteps.factoryStepText,]
+            
+            return map
+        }
+    }
+    
     let title:String
     let footerReusableIdentifier:String
     let headerHeight:CGFloat
@@ -18,5 +30,77 @@ struct NewAddSectionSteps:NewAddSectionProtocol
         self.footerHeight = NewAdd.Constants.footerStepsHeight
         
         self.items = []
+    }
+    
+    //MARK: private
+    
+    private static func factoryItems(build:Build) -> [NewAddSectionItemProtocol]
+    {
+        var items:[NewAddSectionItemProtocol] = []
+        
+        guard
+        
+            let steps:[BuildStep] = build.stepList
+        
+        else
+        {
+            return items
+        }
+        
+        for step:BuildStep in steps
+        {
+            guard
+            
+                let router:((BuildStep) -> (NewAddSectionItemProtocol?)) = NewAddSectionSteps.stepRouters[
+                    step.recipeStepType],
+                let item:NewAddSectionItemProtocol = router(step)
+            
+            else
+            {
+                continue
+            }
+            
+            items.append(item)
+        }
+        
+        return items
+    }
+    
+    private static func factoryStepText(step:BuildStep) -> NewAddSectionItemProtocol?
+    {
+        guard
+        
+            let step:BuildStepText = step as? BuildStepText,
+            let text:String = step.text
+        
+        else
+        {
+            return nil
+        }
+        
+        let item:NewAddSectionItemStepText = NewAddSectionItemStepText(
+            text:text,
+            step:step)
+        
+        return item
+    }
+    
+    private static func factoryStepImage(step:BuildStep) -> NewAddSectionItemProtocol?
+    {
+        guard
+            
+            let step:BuildStepText = step as? BuildStepText,
+            let text:String = step.text
+            
+        else
+        {
+            return nil
+        }
+        
+        let item:NewAddSectionItemStepText = NewAddSectionItemStepText(
+            text:text,
+            step:step)
+        
+        return item
     }
 }
