4eb820b36b3b2aa9f75f47db2eeab2a761b2da65
vauxhall <zero.griffin@gmail.com>
2017-12-18 21:37:14 -0600

    Create steps controller extension

---
+++ Source/Controller/New/ControllerNewAdd+Steps.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+extension ControllerNewAdd
+{
+    //MARK: internal
+    
+    func transitionAddStep()
+    {
+        guard
+            
+            let build:Build = self.model.build,
+            let database:Database = self.model.database
+            
+        else
+        {
+            return
+        }
+        
+        let controller:ControllerNewAddIngredient = ControllerNewAddIngredient(
+            build:build,
+            database:database)
+        
+        self.parentController?.push(
+            controller:controller,
+            horizontal:ControllerTransition.Horizontal.right)
+    }
+}
