5cd7033c30d6c8962db6d4c235788a9f022a4bd3
vauxhall <zero.griffin@gmail.com>
2017-12-19 10:33:15 -0600

    Add gesture routers to list

--- Source/View/NewAdd/ViewNewAddList+Gesture.swift
+++ Source/View/NewAdd/ViewNewAddList+Gesture.swift
@@ -2,12 +2,52 @@ import UIKit
 
 extension ViewNewAddList
 {
-    //private static var 
+    private var gestureRouters:[UIGestureRecognizerState:((UILongPressGestureRecognizer) -> ())]
+    {
+        get
+        {
+            let map:[UIGestureRecognizerState:((UILongPressGestureRecognizer) -> ())] = [
+                UIGestureRecognizerState.began : self.gestureStateBegan,
+                UIGestureRecognizerState.possible : self.gestureStateBegan,
+                UIGestureRecognizerState.changed : self.gestureStateChanged,
+                UIGestureRecognizerState.cancelled : self.gestureStateEnded,
+                UIGestureRecognizerState.ended : self.gestureStateEnded,
+                UIGestureRecognizerState.failed : self.gestureStateEnded]
+            
+            return map
+        }
+    }
+    
+    //MARK: private
+    
+    private func gestureStateBegan(gesture:UILongPressGestureRecognizer)
+    {
+        
+    }
+    
+    private func gestureStateChanged(gesture:UILongPressGestureRecognizer)
+    {
+        
+    }
+    
+    private func gestureStateEnded(gesture:UILongPressGestureRecognizer)
+    {
+        
+    }
     
     //MARK: internal
     
     func gestureReceived(gesture:UILongPressGestureRecognizer)
     {
+        guard
         
+            let router:((UILongPressGestureRecognizer) -> ()) = self.gestureRouters[gesture.state]
+        
+        else
+        {
+            return
+        }
+        
+        router(gesture)
     }
 }
