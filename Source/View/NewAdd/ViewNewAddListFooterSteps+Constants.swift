87f7cd39909ab817b6c4a6be2ba3774a8a516369
vauxhall <zero.griffin@gmail.com>
2017-12-18 21:37:20 -0600

    Transition add step

--- Source/View/NewAdd/ViewNewAddListFooterSteps+Constants.swift
+++ Source/View/NewAdd/ViewNewAddListFooterSteps+Constants.swift
@@ -5,6 +5,6 @@ extension ViewNewAddListFooterSteps
     struct Constants
     {
         static let buttonFontSize:CGFloat = 14
-        static let buttonWidth:CGFloat = 130
+        static let buttonWidth:CGFloat = 110
     }
 }
--- Source/View/NewAdd/ViewNewAddListFooterSteps.swift
+++ Source/View/NewAdd/ViewNewAddListFooterSteps.swift
@@ -2,5 +2,45 @@ import UIKit
 
 final class ViewNewAddListFooterSteps:ViewNewAddListFooter
 {
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let buttonAdd:UIButton = UIButton()
+        buttonAdd.translatesAutoresizingMaskIntoConstraints = false
+        buttonAdd.setTitleColor(
+            UIColor.colourSuccess,
+            for:UIControlState.normal)
+        buttonAdd.setTitleColor(
+            UIColor(white:0, alpha:0.2),
+            for:UIControlState.highlighted)
+        buttonAdd.setTitle(
+            String.localizedView(key:"ViewNewAddListFooterSteps_buttonAdd"),
+            for:UIControlState())
+        buttonAdd.titleLabel!.font = UIFont.medium(
+            size:ViewNewAddListFooterIngredients.Constants.buttonFontSize)
+        buttonAdd.addTarget(
+            self,
+            action:#selector(self.selectorButtonAdd(sender:)),
+            for:UIControlEvents.touchUpInside)
+        self.addSubview(buttonAdd)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:buttonAdd,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonAdd,
+            constant:ViewNewAddListFooterIngredients.Constants.buttonWidth)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonAdd,
+            toView:self)
+    }
     
+    //MARK: selectors
+    
+    @objc
+    private func selectorButtonAdd(sender button:UIButton)
+    {
+        self.controller?.transitionAddStep()
+    }
 }
