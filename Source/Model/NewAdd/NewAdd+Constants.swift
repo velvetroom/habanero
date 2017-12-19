ae360b6796c2ebf53229642387dc6fbf3f639fcc
vauxhall <zero.griffin@gmail.com>
2017-12-18 22:15:42 -0600

    Add step footer assets

--- Source/Model/NewAdd/NewAdd+Constants.swift
+++ Source/Model/NewAdd/NewAdd+Constants.swift
@@ -9,7 +9,7 @@ extension NewAdd
         static let headerStepsHeight:CGFloat = 70
         static let footerInfoHeight:CGFloat = 50
         static let footerIngredientsHeight:CGFloat = 50
-        static let footerStepsHeight:CGFloat = 80
+        static let footerStepsHeight:CGFloat = 100
         static let cellInfoTitleHeight:CGFloat = 80
         static let cellInfoDurationHeight:CGFloat = 140
         static let cellIngredientHeight:CGFloat = 60
--- Source/View/NewAdd/ViewNewAddListFooterSteps+Constants.swift
+++ Source/View/NewAdd/ViewNewAddListFooterSteps+Constants.swift
@@ -4,7 +4,6 @@ extension ViewNewAddListFooterSteps
 {
     struct Constants
     {
-        static let buttonFontSize:CGFloat = 14
         static let buttonWidth:CGFloat = 100
         static let buttonHeight:CGFloat = 50
     }
