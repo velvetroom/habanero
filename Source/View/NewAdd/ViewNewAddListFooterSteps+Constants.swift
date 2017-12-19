69861d88e38813cdd96397c3294818aa8e165d92
vauxhall <zero.griffin@gmail.com>
2017-12-18 21:35:18 -0600

    Add footer steps constants

---
+++ Source/View/NewAdd/ViewNewAddListFooterSteps+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddListFooterSteps
+{
+    struct Constants
+    {
+        static let buttonFontSize:CGFloat = 14
+        static let buttonWidth:CGFloat = 130
+    }
+}
