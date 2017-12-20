f7177b3720c67d3ed4f61d174e0829463f4383ba
vauxhall <zero.griffin@gmail.com>
2017-12-20 00:31:54 -0600

    Factory photo options

--- Source/Model/Photo/Photo+Factory.swift
+++ Source/Model/Photo/Photo+Factory.swift
@@ -3,4 +3,14 @@ import Foundation
 extension Photo
 {
     //MARK: internal
+    
+    class func factoryOptions() -> [PhotoOptionProtocol]
+    {
+        let optionCancel:PhotoOptionCancel = PhotoOptionCancel()
+        
+        let options:[PhotoOptionProtocol] = [
+            optionCancel]
+        
+        return options
+    }
 }
--- Source/Model/Photo/Photo.swift
+++ Source/Model/Photo/Photo.swift
@@ -3,4 +3,12 @@ import UIKit
 final class Photo:Model<ArchPhoto>
 {
     var completion:((UIImage?) -> ())?
+    let options:[PhotoOptionProtocol]
+    
+    required init()
+    {
+        self.options = Photo.factoryOptions()
+        
+        super.init()
+    }
 }
