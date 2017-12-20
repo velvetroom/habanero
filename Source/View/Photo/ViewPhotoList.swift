0679459b591d478ceb4705a1d015950c0f9354f9
vauxhall <zero.griffin@gmail.com>
2017-12-20 01:40:57 -0600

    Add sections inset

--- Source/View/Photo/ViewPhotoList.swift
+++ Source/View/Photo/ViewPhotoList.swift
@@ -2,5 +2,56 @@ import UIKit
 
 final class ViewPhotoList:ViewCollection<ArchPhoto, ViewPhotoListCell, ViewCollectionFlow>
 {
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        self.collectionView.isScrollEnabled = false
+        self.collectionView.bounces = false
+        
+        self.flow.itemSize = CGSize(
+            width:ViewPhotoList.Constants.cellWidth,
+            height:ViewPhotoList.Constants.cellHeight)
+    }
     
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        insetForSectionAt section:Int) -> UIEdgeInsets
+    {
+        let width:CGFloat = collectionView.bounds.width
+        let height:CGFloat = collectionView.bounds.height
+        let cells:Int = self.collectionView.numberOfItems(inSection:0)
+        let cellsHeight:CGFloat = CGFloat(cells) * ViewPhotoList.Constants.cellWidth
+        let remainWidth:CGFloat = width - ViewPhotoList.Constants.cellWidth
+        let marginLeft:CGFloat = remainWidth / 2.0
+        let remainHeight:CGFloat = height - cellsHeight
+        let marginTop:CGFloat = remainHeight / 2.0
+        
+        let sectionInsets:UIEdgeInsets = UIEdgeInsets(
+            top:marginTop,
+            left:marginLeft,
+            bottom:0,
+            right:0)
+        
+        return sectionInsets
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = self.controller.model.options.count
+        
+        return count
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:ViewPhotoListCell = self.cellAtIndex(indexPath:indexPath)
+        
+        return cell
+    }
 }
