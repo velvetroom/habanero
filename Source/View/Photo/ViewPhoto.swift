942c4e9e31ab23881adbddae96dc253ed860fbf6
vauxhall <zero.griffin@gmail.com>
2017-12-19 23:55:10 -0600

    Add blur to view photo

--- Source/View/Photo/ViewPhoto.swift
+++ Source/View/Photo/ViewPhoto.swift
@@ -2,5 +2,17 @@ import UIKit
 
 final class ViewPhoto:ViewMain<ArchPhoto>
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        self.backgroundColor = UIColor.clear
+        
+        let viewBlur:ViewBlur = ViewBlur.light()
+        
+        self.addSubview(ViewBlur)
+        
+        NSLayoutConstraint.equals(
+            view:ViewBlur,
+            toView:self)
+    }
 }
