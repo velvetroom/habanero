28a16983f09e9653e53df1c952aaca44864b9fb7
vauxhall <zero.griffin@gmail.com>
2017-12-20 00:29:51 -0600

    Update option cancel

--- Source/Model/Photo/PhotoOptionCancel.swift
+++ Source/Model/Photo/PhotoOptionCancel.swift
@@ -2,5 +2,6 @@ import UIKit
 
 struct PhotoOptionCancel:PhotoOptionProtocol
 {
-    
+    let icon:UIImage = #imageLiteral(resourceName: "assetPhotoOptionCancel")
+    let router:((ControllerPhoto) -> () -> ()) = ControllerPhoto.optionCancel
 }
