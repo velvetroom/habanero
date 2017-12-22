3c0c4907430b9f8fc81550f866f9f74c32520fc9
vauxhall <zero.griffin@gmail.com>
2017-12-21 23:48:41 -0600

    Present picker controller

--- Source/Controller/Photo/ControllerPhoto+Options.swift
+++ Source/Controller/Photo/ControllerPhoto+Options.swift
@@ -1,4 +1,4 @@
-import Foundation
+import UIKit
 
 extension ControllerPhoto
 {
@@ -11,7 +11,11 @@ extension ControllerPhoto
     
     func optionCamera()
     {
-        
+        let controller:UIImagePickerController = UIImagePickerController()
+        self.parentController?.present(
+            controller,
+            animated:true,
+            completion:nil)
     }
     
     func optionLibrary()
