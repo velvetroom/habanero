ec13837ec15e6d1540698d695041a974ef28d7dc
vauxhall <zero.griffin@gmail.com>
2017-12-22 19:51:11 -0600

    Hide status bar on crop

--- Source/Controller/Photo/ControllerPhotoCrop.swift
+++ Source/Controller/Photo/ControllerPhotoCrop.swift
@@ -2,6 +2,14 @@ import UIKit
 
 final class ControllerPhotoCrop:Controller<ArchPhotoCrop>
 {
+    override var prefersStatusBarHidden:Bool
+    {
+        get
+        {
+            return true
+        }
+    }
+    
     init(image:UIImage)
     {
         super.init()
