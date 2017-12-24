e80a9398852874bba6c6c1f29a5d376690c3a4e4
vauxhall <zero.griffin@gmail.com>
2017-12-23 21:18:45 -0600

    Draw width context top left

--- Source/View/PhotoCrop/ViewPhotoCropDisplayCornerTopLeft.swift
+++ Source/View/PhotoCrop/ViewPhotoCropDisplayCornerTopLeft.swift
@@ -2,5 +2,28 @@ import UIKit
 
 final class ViewPhotoCropDisplayCornerTopLeft:ViewPhotoCropDisplayCorner
 {
-    
+    override func drawWithContext(
+        context:CGContext,
+        width:CGFloat,
+        height:CGFloat)
+    {
+        super.drawWithContext(
+            context:context,
+            width:width,
+            height:height)
+        
+        let initialPoint:CGPoint = CGPoint(
+            x:self.lineWidth_2,
+            y:height)
+        let middlePoint:CGPoint = CGPoint(
+            x:self.lineWidth_2,
+            y:self.lineWidth_2)
+        let endPoint:CGPoint = CGPoint(
+            x:width,
+            y:self.lineWidth_2)
+        
+        context.move(to:initialPoint)
+        context.addLine(to:middlePoint)
+        context.addLine(to:endPoint)
+    }
 }
