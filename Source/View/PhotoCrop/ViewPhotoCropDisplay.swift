80c226ecf3e8302d4261dc5d89361c3e50755b95
vauxhall <zero.griffin@gmail.com>
2017-12-23 21:29:15 -0600

    Layout view corners

--- Source/View/PhotoCrop/ViewPhotoCropDisplay.swift
+++ Source/View/PhotoCrop/ViewPhotoCropDisplay.swift
@@ -3,6 +3,10 @@ import UIKit
 final class ViewPhotoCropDisplay:View<ArchPhotoCrop>
 {
     private(set) weak var viewImage:ViewPhotoCropDisplayImage!
+    private(set) weak var viewCornerTopLeft:ViewPhotoCropDisplayCornerTopLeft!
+    private(set) weak var viewCornerTopRight:ViewPhotoCropDisplayCornerTopRight!
+    private(set) weak var viewCornerBottomLeft:ViewPhotoCropDisplayCornerBottomLeft!
+    private(set) weak var viewCornerBottomRight:ViewPhotoCropDisplayCornerBottomRight!
     
     override func factoryViews()
     {
@@ -11,7 +15,28 @@ final class ViewPhotoCropDisplay:View<ArchPhotoCrop>
         let viewImage:ViewPhotoCropDisplayImage = ViewPhotoCropDisplayImage(controller:self.controller)
         self.viewImage = viewImage
         
+        let viewCornerTopLeft:ViewPhotoCropDisplayCornerTopLeft = ViewPhotoCropDisplayCornerTopLeft()
+        self.viewCornerTopLeft = viewCornerTopLeft
+        
+        let viewCornerTopRight:ViewPhotoCropDisplayCornerTopRight = ViewPhotoCropDisplayCornerTopRight()
+        self.viewCornerTopRight = viewCornerTopRight
+        
+        let viewCornerBottomLeft:ViewPhotoCropDisplayCornerBottomLeft = ViewPhotoCropDisplayCornerBottomLeft()
+        self.viewCornerBottomLeft = viewCornerBottomLeft
+        
+        let viewCornerBottomRight:ViewPhotoCropDisplayCornerBottomRight = ViewPhotoCropDisplayCornerBottomRight()
+        self.viewCornerBottomRight = viewCornerBottomRight
+        
         self.addSubview(viewImage)
+        self.addSubview(viewCornerTopLeft)
+        self.addSubview(viewCornerTopRight)
+        self.addSubview(viewCornerBottomLeft)
+        self.addSubview(viewCornerBottomRight)
+        
+        self.layoutCorner(viewCorner:viewCornerTopLeft)
+        self.layoutCorner(viewCorner:viewCornerTopRight)
+        self.layoutCorner(viewCorner:viewCornerBottomLeft)
+        self.layoutCorner(viewCorner:viewCornerBottomRight)
         
         viewImage.layoutTop = NSLayoutConstraint.topToTop(
             view:viewImage,
@@ -25,5 +50,22 @@ final class ViewPhotoCropDisplay:View<ArchPhotoCrop>
         viewImage.layoutBottom = NSLayoutConstraint.bottomToBottom(
             view:viewImage,
             toView:self)
+    }
+    
+    //MARK: private
+    
+    private func layoutCorner(viewCorner:ViewPhotoCropDisplayCorner)
+    {
+        self.addSubview(viewCorner)
+        
+        viewCorner.layoutTop = NSLayoutConstraint.topToTop(
+            view:viewCorner,
+            toView:self)
+        viewCorner.layoutLeft = NSLayoutConstraint.leftToLeft(
+            view:viewCorner,
+            toView:self)
+        NSLayoutConstraint.size(
+            view:viewCorner,
+            constant:ViewPhotoCropDisplayCorner.Constant.cornerSize)
     }
 }
--- Source/View/PhotoCrop/ViewPhotoCropDisplayCorner+Constants.swift
+++ Source/View/PhotoCrop/ViewPhotoCropDisplayCorner+Constants.swift
@@ -5,5 +5,6 @@ extension ViewPhotoCropDisplayCorner
     struct Constant
     {
         static let lineWidth:CGFloat = 4
+        static let cornerSize:CGFloat = 40
     }
 }
