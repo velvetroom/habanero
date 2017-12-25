f3dab12d527c6658ad1ae38b2003387cd0830732
vauxhall <zero.griffin@gmail.com>
2017-12-25 00:01:09 -0600

    Listen to touches

--- Source/Controller/Photo/ControllerPhotoCrop.swift
+++ Source/Controller/Photo/ControllerPhotoCrop.swift
@@ -51,6 +51,34 @@ final class ControllerPhotoCrop:Controller<ArchPhotoCrop>
         }
     }
     
+    override func touchesBegan(
+        _ touches:Set<UITouch>,
+        with event:UIEvent?)
+    {
+        
+    }
+    
+    override func touchesMoved(
+        _ touches:Set<UITouch>,
+        with event:UIEvent?)
+    {
+        
+    }
+    
+    override func touchesCancelled(
+        _ touches:Set<UITouch>,
+        with event:UIEvent?)
+    {
+        
+    }
+    
+    override func touchesEnded(
+        _ touches:Set<UITouch>,
+        with event:UIEvent?)
+    {
+        
+    }
+    
     //MARK: internal
     
     func transitionBack()
