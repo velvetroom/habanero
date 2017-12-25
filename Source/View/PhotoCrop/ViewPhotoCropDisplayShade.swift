1ba363f078d56c7be3db6350d5142dbeb85b7c61
vauxhall <zero.griffin@gmail.com>
2017-12-24 22:06:33 -0600

    Add shape layer to share

--- Source/View/PhotoCrop/ViewPhotoCropDisplayShade.swift
+++ Source/View/PhotoCrop/ViewPhotoCropDisplayShade.swift
@@ -1,6 +1,24 @@
 import UIKit
 
-final class ViewPhotoCropDisplayShade:View
+final class ViewPhotoCropDisplayShade:View<ArchPhotoCrop>
 {
+    private weak var layerMask:CAShapeLayer!
     
+    override func factoryViews()
+    {
+        super.factoryViews()
+        self.isUserInteractionEnabled = false
+        
+        let layerMask:CAShapeLayer = CAShapeLayer()
+        layerMask.fillRule = kCAFillRuleEvenOdd
+        layerMask.fillColor = UIColor(white:0, alpha:0.8)
+        self.layerMask = layerMask
+        
+        self.layer.addSublayer(layerMask)
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+    }
 }
