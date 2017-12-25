21462f70c3f51943ef0f30165cb1de16f148dcd9
vauxhall <zero.griffin@gmail.com>
2017-12-24 22:27:25 -0600

    Compute inner rect constraints

--- Source/View/PhotoCrop/ViewPhotoCropDisplayShade.swift
+++ Source/View/PhotoCrop/ViewPhotoCropDisplayShade.swift
@@ -4,6 +4,46 @@ final class ViewPhotoCropDisplayShade:View<ArchPhotoCrop>
 {
     private weak var layerMask:CAShapeLayer!
     
+    private var innerRectMinY:CGFloat
+    {
+        get
+        {
+            let minY:CGFloat = self.controller.viewMain.viewDisplay.viewCornerTopLeft.layoutTop.constant
+            
+            return minY
+        }
+    }
+    
+    private var innerRectMaxY:CGFloat
+    {
+        get
+        {
+            let maxY:CGFloat = self.controller.viewMain.viewDisplay.viewCornerTopRight.layoutTop.constant
+            
+            return maxY
+        }
+    }
+    
+    private var innerRectMinX:CGFloat
+    {
+        get
+        {
+            let minX:CGFloat = self.controller.viewMain.viewDisplay.viewCornerTopLeft.layoutLeft.constant
+            
+            return minX
+        }
+    }
+    
+    private var innerRectMaxX:CGFloat
+    {
+        get
+        {
+            let maxX:CGFloat = self.controller.viewMain.viewDisplay.viewCornerTopRight.layoutLeft.constant
+            
+            return maxX
+        }
+    }
+    
     override func factoryViews()
     {
         super.factoryViews()
@@ -19,6 +59,32 @@ final class ViewPhotoCropDisplayShade:View<ArchPhotoCrop>
     
     override func layoutSubviews()
     {
+        self.updateMaskDisplay()
+        
         super.layoutSubviews()
+    }
+    
+    //MARK: private
+    
+    private func updateMaskDisplay()
+    {
+        let outerRect:CGRect = self.bounds
+        let outerWidth:CGFloat = outerRect.width
+        let outerHeight:CGFloat = outerRect.height
+        
+        
+        
+        let outerPath:UIBezierPath = UIBezierPath(rect:outerRect)
+        
+        
+        let highlightPath:UIBezierPath = UIBezierPath(roundedRect:highlightFrame, cornerRadius:0)
+        backgroundPath.append(highlightPath)
+        backgroundPath.usesEvenOddFillRule = true
+        
+        let fillLayer:CAShapeLayer = CAShapeLayer()
+        fillLayer.fillRule = kCAFillRuleEvenOdd
+        fillLayer.fillColor = UIColor.black.cgColor
+        fillLayer.path = backgroundPath.cgPath
+        layer.addSublayer(fillLayer)
     }
 }
