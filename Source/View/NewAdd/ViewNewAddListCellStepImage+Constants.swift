e5debb0bc1d5e3a57dc708d5265a990b438b0a8c
vauxhall <zero.griffin@gmail.com>
2017-12-27 23:31:33 -0600

    Create cell image constants extension

---
+++ Source/View/NewAdd/ViewNewAddListCellStepImage+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension ViewNewAddListCellStepImage
+{
+    struct Constants
+    {
+        static let fontSize:CGFloat = 16
+        static let buttonWidth:CGFloat = 50
+    }
+}
