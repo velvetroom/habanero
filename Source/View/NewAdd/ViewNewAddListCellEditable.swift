c6890cfa3812c2b5fe7c714d358a20d311bea00d
vauxhall <zero.griffin@gmail.com>
2017-12-30 21:23:23 -0600

    Create base cell view editable

---
+++ Source/View/NewAdd/ViewNewAddListCellEditable.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+class ViewNewAddListCellEditable:ViewNewAddListCell
+{
+    private weak var button:UIButton!
+    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(
+            self,
+            action:#selector(self.selectorButtonTouch(sender:)),
+            for:UIControlEvents.touchUpInside)
+        button.addTarget(
+            self,
+            action:#selector(self.selectorButtonHighlighted(sender:)),
+            for:UIControlEvents.touchDown)
+        button.addTarget(
+            self,
+            action:#selector(self.selectorButtonReleased(sender:)),
+            for:UIControlEvents([
+                UIControlEvents.touchCancel,
+                UIControlEvents.touchUpOutside]))
+        self.button = button
+
+        self.addSubview(button)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
+    }
+    
+    override func showHighlighted()
+    {
+        super.showHighlighted()
+        
+        self.button.backgroundColor = UIColor(white:1, alpha:0.9)
+    }
+    
+    override func showReleased()
+    {
+        super.showReleased()
+        
+        self.button.backgroundColor = UIColor.clear
+    }
+    
+    //MARK: selectors
+    
+    @objc
+    private func selectorButtonTouch(sender button:UIButton)
+    {
+        self.showReleased()
+        self.selectorEdit()
+    }
+    
+    @objc
+    private func selectorButtonHighlighted(sender button:UIButton)
+    {
+        self.showHighlighted()
+    }
+    
+    @objc
+    private func selectorButtonReleased(sender button:UIButton)
+    {
+        self.showReleased()
+    }
+    
+    //MARK: internal
+    
+    func selectorEdit() { }
+}
