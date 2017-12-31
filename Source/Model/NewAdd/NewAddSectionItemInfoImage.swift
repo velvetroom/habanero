d49ecf72f5af0fd18dde7f1115d7096abae0b42d
vauxhall <zero.griffin@gmail.com>
2017-12-30 18:02:46 -0600

    Create struct info image

---
+++ Source/Model/NewAdd/NewAddSectionItemInfoImage.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+struct NewAddSectionItemInfoImage:NewAddSectionItemProtocol
+{
+    let reusableIdentifier:String = ViewNewAddListCellInfoTitle.reusableIdentifier
+    let cellHeight:CGFloat = NewAdd.Constants.cellInfoImageHeight
+    let title:String
+}
