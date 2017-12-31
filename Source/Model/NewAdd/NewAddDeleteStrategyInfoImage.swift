9903035fe65e99f9b83abc2dde2baa9105e6b125
vauxhall <zero.griffin@gmail.com>
2017-12-31 13:34:31 -0600

    Create strategy info image

---
+++ Source/Model/NewAdd/NewAddDeleteStrategyInfoImage.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct NewAddDeleteStrategyInfoImage:NewAddDeleteStrategyProtocol
+{
+    //MARK: internal
+    
+    static func delete(
+        build:Build,
+        database:Database,
+        completion:@escaping (() -> ()))
+    {
+        
+    }
+}
