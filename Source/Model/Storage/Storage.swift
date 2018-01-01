34d909b9ca4d1c6101999b49be2855c6a2d6caf6
vauxhall <zero.griffin@gmail.com>
2017-12-31 19:07:14 -0600

    Start firebase storage provider

--- Source/Model/Storage/Storage.swift
+++ Source/Model/Storage/Storage.swift
@@ -2,5 +2,10 @@ import Foundation
 
 final class Storage
 {
+    let provider:StorageProviderProtocol
     
+    init(provider:StorageProviderProtocol = StorageProviderFirebase())
+    {
+        self.provider = provider
+    }
 }
--- Source/Model/StorageProvider/StorageProviderFirebase.swift
+++ Source/Model/StorageProvider/StorageProviderFirebase.swift
@@ -1,6 +1,41 @@
 import Foundation
+import FirebaseStorage
 
 final class StorageProviderFirebase:StorageProviderProtocol
 {
+    let reference:StorageReference
     
+    init()
+    {
+        self.reference = FirebaseStorage.Storage.storage().reference()
+    }
+    
+    //MARK: internal
+    
+    func saveData(path:String, data:Data, completionHandler:@escaping((String?) -> ()))
+    {
+        let childReference:StorageReference = reference.child(path)
+        childReference.putData(
+            data,
+            metadata:nil)
+        { (metaData:StorageMetadata?, error:Error?) in
+            
+            let errorString:String? = error?.localizedDescription
+            completionHandler(errorString)
+        }
+    }
+    
+    func loadData(path:String, completionHandler:@escaping((Data?, Error?) -> ()))
+    {
+        let childReference:StorageReference = reference.child(path)
+        childReference.getData(
+            maxSize:kTenMegaBytes,
+            completion:completionHandler)
+    }
+    
+    func deleteData(path:String, completionHandler:@escaping((Error?) -> ()))
+    {
+        let childReference:StorageReference = reference.child(path)
+        childReference.delete(completion:completionHandler)
+    }
 }
