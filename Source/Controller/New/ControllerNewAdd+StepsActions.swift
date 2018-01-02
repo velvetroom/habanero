9683c032e7d6c5af2d98fd3dc4705862f0050a75
vauxhall <zero.griffin@gmail.com>
2018-01-01 20:01:03 -0600

    Show alert before deleting step

--- Source/Controller/New/ControllerNewAdd+StepsActions.swift
+++ Source/Controller/New/ControllerNewAdd+StepsActions.swift
@@ -16,13 +16,52 @@ extension ControllerNewAdd
     
     //MARK: private
     
-    private func deleteStep(step:BuildStep)
+    private func confirmDeleteStep(step:BuildStep)
     {
         self.model.deleteStep(step:step)
         { [weak self] in
             
             self?.loadModel()
         }
+    }
+    
+    private func deleteStep(step:BuildStep)
+    {
+        let alert:UIAlertController = UIAlertController(
+            title:String.localizedController(key:"ControllerNewAdd_alertDeleteStepTitle"),
+            message:nil,
+            preferredStyle:UIAlertControllerStyle.actionSheet)
+        
+        let actionCancel:UIAlertAction = UIAlertAction(
+            title:String.localizedController(key:"ControllerNewAdd_alertDeleteStepCancel"),
+            style:UIAlertActionStyle.cancel)
+        
+        let actionDelete:UIAlertAction = UIAlertAction(
+            title:String.localizedController(key:"ControllerNewAdd_alertDeleteStepDelete"),
+            style:UIAlertActionStyle.destructive)
+        { [weak self] (action:UIAlertAction) in
+            
+            self?.confirmDeleteStep(step:step)
+        }
+        
+        alert.addAction(actionDelete)
+        alert.addAction(actionCancel)
+        
+        if let popover:UIPopoverPresentationController = alert.popoverPresentationController
+        {
+            popover.sourceView = self.view
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+            popover.sourceRect = CGRect(
+                x:self.view.center.x,
+                y:0,
+                width:1,
+                height:1)
+        }
+        
+        self.present(
+            alert,
+            animated:true,
+            completion:nil)
     }
     
     private func stepEditText(step:BuildStepText)
