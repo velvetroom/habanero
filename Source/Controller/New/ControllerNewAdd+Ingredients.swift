1f9fb80d40144c2689241c69dc5b583662db05c3
vauxhall <zero.griffin@gmail.com>
2018-01-01 19:57:38 -0600

    Display alert before deleting ingredient

--- Source/Controller/New/ControllerNewAdd+Ingredients.swift
+++ Source/Controller/New/ControllerNewAdd+Ingredients.swift
@@ -1,18 +1,57 @@
-import Foundation
+import UIKit
 
 extension ControllerNewAdd
 {
-    //MARK: internal
+    //MARK: private
     
-    func removeIngredient(item:NewAddSectionItemIngredient)
+    private func confirmRemoveIngredient(item:NewAddSectionItemIngredient)
     {
-        self.viewMain.viewList.isUserInteractionEnabled = false
-        
         self.model.removeIngredient(item:item)
         { [weak self] in
             
             self?.loadModel()
         }
+    }
+    
+    //MARK: internal
+    
+    func removeIngredient(item:NewAddSectionItemIngredient)
+    {
+        let alert:UIAlertController = UIAlertController(
+            title:String.localizedController(key:"ControllerNewAdd_alertDeleteIngredientTitle"),
+            message:item.name,
+            preferredStyle:UIAlertControllerStyle.actionSheet)
+        
+        let actionCancel:UIAlertAction = UIAlertAction(
+            title:String.localizedController(key:"ControllerNewAdd_alertDeleteIngredientCancel"),
+            style:UIAlertActionStyle.cancel)
+        
+        let actionDelete:UIAlertAction = UIAlertAction(
+            title:String.localizedController(key:"ControllerNewAdd_alertDeleteIngredientDelete"),
+            style:UIAlertActionStyle.destructive)
+        { [weak self] (action:UIAlertAction) in
+            
+            self?.confirmRemoveIngredient(item:item)
+        }
+        
+        alert.addAction(actionDelete)
+        alert.addAction(actionCancel)
+        
+        if let popover:UIPopoverPresentationController = alert.popoverPresentationController
+        {
+            popover.sourceView = self.view
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+            popover.sourceRect = CGRect(
+                x:self.view.center.x,
+                y:0,
+                width:1,
+                height:1)
+        }
+        
+        self.present(
+            alert,
+            animated:true,
+            completion:nil)
     }
     
     func transitionAddIngredient()
