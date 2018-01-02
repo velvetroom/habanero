1ee2536c9387ca36fef130dd9213356f795851a8
vauxhall <zero.griffin@gmail.com>
2018-01-01 19:07:17 -0600

    Increase cell height

--- Source/View/New/ViewNewList+Constants.swift
+++ Source/View/New/ViewNewList+Constants.swift
@@ -5,7 +5,7 @@ extension ViewNewList
     struct Constants
     {
         static let headerHeight:CGFloat = 120
-        static let cellHeight:CGFloat = 55
+        static let cellHeight:CGFloat = 65
         static let collectionBottom:CGFloat = 10
     }
 }
