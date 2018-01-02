468439e76343c1d316c3cb2c21716cb3d960c234
vauxhall <zero.griffin@gmail.com>
2018-01-01 20:04:54 -0600

    Add show alert helper

--- Source/Controller/Abstract/Controller+Alert.swift
+++ Source/Controller/Abstract/Controller+Alert.swift
@@ -3,4 +3,36 @@ import UIKit
 extension Controller
 {
     //MARK: internal
+    
+    func showAlert(
+        title:String?,
+        message:String?
+        actions:[UIAlertAction])
+    {
+        let alert:UIAlertController = UIAlertController(
+            title:title,
+            message:message,
+            preferredStyle:UIAlertControllerStyle.actionSheet)
+        
+        for action:UIAlertAction in actions
+        {
+            alert.addAction(action)
+        }
+        
+        if let popover:UIPopoverPresentationController = alert.popoverPresentationController
+        {
+            popover.sourceView = self.view
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+            popover.sourceRect = CGRect(
+                x:self.view.center.x,
+                y:0,
+                width:1,
+                height:1)
+        }
+        
+        self.present(
+            alert,
+            animated:true,
+            completion:nil)
+    }
 }
