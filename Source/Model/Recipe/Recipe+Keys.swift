59ae90f452a2dc1dba9cd6aa4e0f9e5c69c05a0b
vauxhall <zero.griffin@gmail.com>
2018-01-03 18:29:53 -0600

    Add recipe properties and keys

--- Source/Model/Recipe/Recipe+Keys.swift
+++ Source/Model/Recipe/Recipe+Keys.swift
@@ -4,6 +4,11 @@ extension Recipe
 {
     struct Keys
     {
+        static let created:String = "created"
+        static let duration:String = "duration"
+        static let syncstamp:String = "syncstamp"
         static let status:String = "status"
+        static let title:String = "title"
+        static let subtitle:String = "subtitle"
     }
 }
--- Source/Model/Recipe/Recipe.swift
+++ Source/Model/Recipe/Recipe.swift
@@ -2,8 +2,13 @@ import Foundation
 
 final class Recipe:CloudItemProtocol
 {
+    let created:TimeInterval
+    let duration:TimeInterval
+    let syncstamp:TimeInterval
     let identifier:String
     let parentPath:String
+    let title:String
+    let subtitle:String
     let status:RecipeStatus
     
     init?(
@@ -13,15 +18,26 @@ final class Recipe:CloudItemProtocol
     {
         guard
             
-            let name:String = json[Ingredient.Keys.name] as? String
+            let rawStatus:Int = json[Recipe.Keys.status] as? Int,
+            let status:RecipeStatus = RecipeStatus(rawValue:rawStatus),
+            let created:TimeInterval = json[Recipe.Keys.created] as? TimeInterval,
+            let duration:TimeInterval = json[Recipe.Keys.duration] as? TimeInterval,
+            let syncstamp:TimeInterval = json[Recipe.Keys.syncstamp] as? TimeInterval,
+            let title:String = json[Recipe.Keys.title] as? String,
+            let subtitle:String = json[Recipe.Keys.subtitle] as? String
             
-            else
+        else
         {
             return nil
         }
         
-        self.name = name
         self.parentPath = parentPath
         self.identifier = identifier
+        self.status = status
+        self.created = created
+        self.duration = duration
+        self.syncstamp = syncstamp
+        self.title = title
+        self.subtitle = subtitle
     }
 }
