753e537eaca6b74c27f91db7ee0bc13ca6a2a14c
vauxhall <zero.griffin@gmail.com>
2018-01-03 18:09:23 -0600

    Save on background thread

--- Source/Model/NewAdd/NewAdd+Save.swift
+++ Source/Model/NewAdd/NewAdd+Save.swift
@@ -2,10 +2,31 @@ import Foundation
 
 extension NewAdd
 {
+    //MARK: private
+    
+    private func asyncSave(completion:@escaping((Error?) -> ()))
+    {
+        
+    }
+    
+    private func finishSaving(
+        error:Error?,
+        completion:@escaping((Error?) -> ()))
+    {
+        DispatchQueue.main.async
+        {
+            completion(error)
+        }
+    }
+    
     //MARK: internal
     
     func save(completion:@escaping((Error?) -> ()))
     {
-        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.asyncSave(completion:completion)
+        }
     }
 }
