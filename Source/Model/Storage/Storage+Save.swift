3fb32cf0d6226464c3a73f69c2f87ff29cc3cea1
vauxhall <zero.griffin@gmail.com>
2018-01-03 19:51:59 -0600

    Save recipe image

--- Source/Model/Storage/Storage+Save.swift
+++ Source/Model/Storage/Storage+Save.swift
@@ -2,7 +2,46 @@ import Foundation
 
 extension Storage
 {
+    //MARK: private
+    
+    private class func dataFor(fileLocation:URL) -> Data?
+    {
+        let data:Data
+        
+        do
+        {
+            try data = Data(
+                contentsOf:fileLocation,
+                options:Data.ReadingOptions.mappedIfSafe)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        return data
+    }
+    
     //MARK: internal
     
-    
+    func saveRecipeImage(
+        build:Build,
+        completion:@escaping((Error?) -> ()))
+    {
+        guard
+            
+            let imageIdentifier:String = build.imageIdentifier,
+            let imageURL:URL = NewAdd.localURLForImage(identifier:imageIdentifier),
+            let data:Data = Storage.dataFor(fileLocation:imageURL)
+        
+        else
+        {
+            return
+        }
+        
+        self.provider.save(
+            data:data,
+            at:StorageContainer.recipe,
+            completion:completion)
+    }
 }
