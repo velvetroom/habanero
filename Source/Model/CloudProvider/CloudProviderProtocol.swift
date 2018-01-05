5945e948c4fb2a6b58324f243e59c2fba15b05f9
vauxhall <zero.griffin@gmail.com>
2018-01-04 21:06:50 -0600

    Update cloud provider protocol

--- Source/Model/CloudProvider/CloudProviderProtocol.swift
+++ Source/Model/CloudProvider/CloudProviderProtocol.swift
@@ -3,17 +3,26 @@ import Foundation
 protocol CloudProviderProtocol
 {
     func createItemAt(
-        entity:CloudEntityProtocol,
+        listType:CloudListProtocol.Type,
         with json:[String:Any],
         completion:@escaping((String?, Error?) -> ()))
     
-    func loadList<T:CloudListProtocol>(
-        identifier:String,
-        completion:@escaping((T?, Error?) -> ()))
+    func createItemAt(
+        listKey:String,
+        of item:CloudItemProtocol,
+        with json:[String:Any],
+        completion:@escaping((String?, Error?) -> ()))
+    
+    func createItemAt(
+        list:CloudListProtocol,
+        with json:[String:Any],
+        completion:@escaping((String?, Error?) -> ()))
+    
+    func loadList<T:CloudListProtocol>(completion:@escaping((T?, Error?) -> ()))
     
     func loadList<T:CloudListProtocol>(
-        identifier:String,
-        of entity:CloudEntityProtocol,
+        listKey:String,
+        of item:CloudItemProtocol,
         completion:@escaping((T?, Error?) -> ()))
     
     func loadItem<T:CloudItemProtocol>(
