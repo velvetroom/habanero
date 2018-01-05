493bcfaf91a227915c3c2666aec0654060927567
vauxhall <zero.griffin@gmail.com>
2018-01-04 20:07:30 -0600

    Add save steps extension

---
+++ Source/Model/NewAdd/NewAdd+SaveSteps.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension NewAdd
+{
+    //MARK: internal
+    
+    func createSteps(
+        recipeIdentifier:String,
+        completion:@escaping((Error?) -> ()))
+    {
+        
+    }
+}
