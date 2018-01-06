574bcfc06f03d3c2904e0d84c652a96c4d81da6f
vauxhall <zero.griffin@gmail.com>
2018-01-05 20:21:44 -0600

    Save steps list

--- Source/Model/NewAdd/NewAdd+SaveSteps.swift
+++ Source/Model/NewAdd/NewAdd+SaveSteps.swift
@@ -2,12 +2,32 @@ import Foundation
 
 extension NewAdd
 {
+    private func saveSteps(
+        steps:[BuildStep],
+        for recipe:Recipe,
+        completion:@escaping((Error?) -> ()))
+    {
+        
+    }
+    
     //MARK: internal
     
     func saveSteps(
         recipe:Recipe,
         completion:@escaping((Error?) -> ()))
     {
+        guard
+            
+            let sateps:[BuildStep] = self.build?.stepList
+            
+        else
+        {
+            return
+        }
         
+        self.saveSteps(
+            steps:steps,
+            for:recipe,
+            completion:completion)
     }
 }
