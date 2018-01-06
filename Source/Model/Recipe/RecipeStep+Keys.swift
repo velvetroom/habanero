06a8c08fa427f1fa49da251de868d279cad031b5
vauxhall <zero.griffin@gmail.com>
2018-01-06 15:20:09 -0600

    Create recipe step keys extension

---
+++ Source/Model/Recipe/RecipeStep+Keys.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension RecipeStep
+{
+    struct Keys
+    {
+        static let stepType:String = "stepType"
+        static let text:String = "text"
+        static let image:String = "image"
+    }
+}
