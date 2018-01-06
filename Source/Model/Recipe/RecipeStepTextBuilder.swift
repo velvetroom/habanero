455222c066276d903b8276292c4de1d4e1b72932
vauxhall <zero.griffin@gmail.com>
2018-01-06 15:44:15 -0600

    Serialize recipe step text

--- Source/Model/Recipe/RecipeStepTextBuilder.swift
+++ Source/Model/Recipe/RecipeStepTextBuilder.swift
@@ -2,20 +2,15 @@ import Foundation
 
 struct RecipeStepTextBuilder:CloudItemBuilderProtocol
 {
-    var stepType:IngredientAmountType
-    var ingredientIdentifier:String
-    var ingredientName:String
-    var cups:Float
+    var text:String
     
     var json:[String:Any]
     {
         get
         {
             let json:[String:Any] = [
-                RecipeIngredient.Keys.cups : self.cups,
-                RecipeIngredient.Keys.amountType : self.amountType.rawValue,
-                RecipeIngredient.Keys.ingredientIdentifier : self.ingredientIdentifier,
-                RecipeIngredient.Keys.ingredientName : self.ingredientName]
+                RecipeStep.Keys.stepType : RecipeStepType.text,
+                RecipeStep.Keys.text : self.text]
             
             return json
         }
@@ -23,9 +18,6 @@ struct RecipeStepTextBuilder:CloudItemBuilderProtocol
     
     init()
     {
-        self.amountType = IngredientAmountType.unknown
-        self.ingredientIdentifier = String()
-        self.ingredientName = String()
-        self.cups = 0
+        self.text = String()
     }
 }
