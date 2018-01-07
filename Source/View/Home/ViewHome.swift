25e04cab5d57fb5b35ee64a2b7b29d2ec7507595
vauxhall <zero.griffin@gmail.com>
2018-01-07 17:02:29 -0600

    Add view bar to home

--- Source/View/Home/ViewHome.swift
+++ Source/View/Home/ViewHome.swift
@@ -8,13 +8,26 @@ final class ViewHome:ViewMain<ArchHome>
     {
         super.factoryViews()
         
+        let viewBar:ViewHomeBar = ViewHomeBar(controller:self.controller)
+        
         let viewSpinner:ViewSpinner = ViewSpinner()
         self.viewSpinner = viewSpinner
         
         self.addSubview(viewSpinner)
+        self.addSubview(viewBar)
         
         NSLayoutConstraint.equals(
             view:viewSpinner,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:ViewGlobal.Constants.barHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
             toView:self)
     }
     
--- Source/View/Home/ViewHomeBar.swift
+++ Source/View/Home/ViewHomeBar.swift
@@ -5,6 +5,6 @@ final class ViewHomeBar:ViewBar<ArchHome>
     override func factoryViews()
     {
         super.factoryViews()
-        
+        self.labelTitle.text = String.localizedView(key:"ViewHomeBar_labelTitle")
     }
 }
