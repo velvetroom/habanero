103aad18de73abafe8030bd0cb31080cbfcd07ff
vauxhall <zero.griffin@gmail.com>
2018-01-07 17:36:38 -0600

    Add storage and cloud to recipes

--- Source/Model/Home/Home.swift
+++ Source/Model/Home/Home.swift
@@ -4,4 +4,16 @@ final class Home:Model<ArchHome>
 {
     var settings:Settings?
     var database:Database?
+    var recipes:[Recipe]
+    let cloud:Cloud
+    let storage:Storage
+    
+    required init()
+    {
+        self.cloud = Cloud()
+        self.storage = Storage()
+        self.recipes = []
+        
+        super.init()
+    }
 }
