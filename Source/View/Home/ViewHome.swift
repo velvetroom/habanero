acfbea3dd640ebf40ce8ed435368a025792e3afa
vauxhall <zero.griffin@gmail.com>
2018-01-07 16:56:03 -0600

    Add spinner to home view

--- Source/View/Home/ViewHome.swift
+++ Source/View/Home/ViewHome.swift
@@ -2,5 +2,19 @@ import UIKit
 
 final class ViewHome:ViewMain<ArchHome>
 {
-
+    private weak var viewSpinner:ViewSpinner!
+    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let viewSpinner:ViewSpinner = ViewSpinner()
+        self.viewSpinner = viewSpinner
+        
+        self.addSubview(viewSpinner)
+        
+        NSLayoutConstraint.equals(
+            view:viewSpinner,
+            toView:self)
+    }
 }
