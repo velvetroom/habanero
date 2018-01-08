db86c26a40c2817fee3cf89b0274706cd94e3677
vauxhall <zero.griffin@gmail.com>
2018-01-08 16:05:47 -0600

    Load recipes on home

--- Source/Model/Home/Home+Recipes.swift
+++ Source/Model/Home/Home+Recipes.swift
@@ -6,7 +6,54 @@ extension Home
     
     private func asyncLoadRecipes(completion:@escaping(() -> ()))
     {
+        self.cloud.loadRecipes
+        { [weak self] (recipeList:RecipeList?, error:Error?) in
+            
+            guard
+            
+                error == nil,
+                let recipeList:RecipeList = recipeList
+            
+            else
+            {
+                self?.recipesLoaded(
+                    recipes:[],
+                    completion:completion)
+                
+                return
+            }
+            
+            self?.sortRecipes(
+                recipeList:recipeList,
+                completion:completion)
+        }
+    }
+    
+    private func sortRecipes(
+        recipeList:RecipeList,
+        completion:@escaping(() -> ()))
+    {
+        let recipes:[Recipe] = recipeList.items.sorted
+        { (recipeA:Recipe, recipeB:Recipe) -> Bool in
+            
+            return recipeA.created <= recipeB.created
+        }
         
+        self.recipesLoaded(
+            recipes:recipes,
+            completion:completion)
+    }
+    
+    private func recipesLoaded(
+        recipes:[Recipe],
+        completion:@escaping(() -> ()))
+    {
+        self.recipes = recipes
+        
+        DispatchQueue.main.async
+        {
+            completion()
+        }
     }
     
     //MARK: internal
