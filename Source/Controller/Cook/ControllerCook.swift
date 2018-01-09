54b80456c7bd72d43d39a0a0d2041e0b7d3dadfb
vauxhall <zero.griffin@gmail.com>
2018-01-09 17:40:28 -0600

    Init cook with recipe

--- Source/Controller/Cook/ControllerCook.swift
+++ Source/Controller/Cook/ControllerCook.swift
@@ -2,5 +2,15 @@ import Foundation
 
 final class ControllerCook:Controller<ArchCook>
 {
+    init(recipe:Recipe)
+    {
+        super.init()
+        
+        self.model.recipe = recipe
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
