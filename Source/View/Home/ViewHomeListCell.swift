5e869d3f166592b6056b973abe9c4db73d1c5826
vauxhall <zero.griffin@gmail.com>
2018-01-08 18:38:33 -0600

    Add base view to home cell

--- Source/View/Home/ViewHomeListCell.swift
+++ Source/View/Home/ViewHomeListCell.swift
@@ -2,5 +2,25 @@ import UIKit
 
 final class ViewHomeListCell:ViewCollectionCell
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let baseView:UIView = UIView()
+        baseView.isUserInteractionEnabled = false
+        baseView.backgroundColor = UIColor.white
+        baseView.clipsToBounds = true
+        baseView.translatesAutoresizingMaskIntoConstraints = false
+        baseView.layer.cornerRadius = ViewHomeListCell.Constants.cornerRadius
+        
+        self.addSubview(baseView)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:baseView,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:baseView,
+            toView:self,
+            margin:ViewHomeList.Constants.padding)
+    }
 }
