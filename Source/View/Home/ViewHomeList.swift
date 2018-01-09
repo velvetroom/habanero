986eaf0fd4d9d9166e325731f4d1b08d5699e335
vauxhall <zero.griffin@gmail.com>
2018-01-08 18:28:45 -0600

    Add cell size in home

--- Source/View/Home/ViewHomeList.swift
+++ Source/View/Home/ViewHomeList.swift
@@ -2,5 +2,41 @@ import UIKit
 
 final class ViewHomeList:ViewCollection<ArchHome, ViewHomeListCell, UICollectionViewFlowLayout>
 {
+    private var cellSize:CGSize?
     
+    
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let width:CGFloat = collectionView.bounds.width
+            
+            let cellSize:CGSize = CGSize(
+                width:width,
+                height:width)
+            
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = self.controller.model.items.count
+        
+        return count
+    }
 }
