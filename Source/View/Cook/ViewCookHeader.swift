4d57f024e8a2dc97de0d2fa79f69d1520ab151af
vauxhall <zero.griffin@gmail.com>
2018-01-09 23:05:51 -0600

    Layout image on header

--- Source/View/Cook/ViewCookHeader.swift
+++ Source/View/Cook/ViewCookHeader.swift
@@ -6,5 +6,22 @@ final class ViewCookHeader:View<ArchCook>
     {
         super.factoryViews()
         self.isUserInteractionEnabled = false
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.image = self.controller.model.item?.image
+        
+        self.addSubview(imageView)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageView,
+            toView:self,
+            margin:ViewCookHeader.Constants.verticalMargin)
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
     }
 }
