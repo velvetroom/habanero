bc480f76147c00d284e7cbc3066f63ed2a8de3a1
vauxhall <zero.griffin@gmail.com>
2018-01-09 23:15:50 -0600

    Layout header in cook

--- Source/View/Cook/ViewCook.swift
+++ Source/View/Cook/ViewCook.swift
@@ -2,10 +2,40 @@ import UIKit
 
 final class ViewCook:ViewMain<ArchCook>
 {
+    private weak var layoutHeaderHeight:NSLayoutConstraint!
+    
     override func factoryViews()
     {
         super.factoryViews()
-        
         self.shouldPanBack = true
+        
+        let viewHeader:ViewCookHeader = ViewCookHeader(controller:self.controller)
+        
+        self.addSubview(viewHeader)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewHeader,
+            toView:self)
+        layoutHeaderHeight = NSLayoutConstraint.height(
+            view:viewHeader)
+        NSLayoutConstraint.equals(
+            view:viewHeader,
+            toView:self)
+    }
+    
+    override func layoutSubviews()
+    {
+        self.adjustHeaderHeight()
+        
+        super.layoutSubviews()
+    }
+    
+    //MARK: internal
+    
+    func adjustHeaderHeight()
+    {
+        let width:CGFloat = self.bounds.width
+        
+        layoutHeaderHeight.constant = width
     }
 }
