53b1a72ba5bb8fc1abac215577f4771db1950b4a
vauxhall <zero.griffin@gmail.com>
2018-01-11 16:36:02 -0600

    Factory cook items

--- Source/Model/Cook/Cook+Factory.swift
+++ Source/Model/Cook/Cook+Factory.swift
@@ -2,7 +2,27 @@ import Foundation
 
 extension Cook
 {
+    //MARK: private
+    
+    private class func factoryIngredientItems(recipe:Recipe) -> [CookItemProtocol]
+    {
+        return []
+    }
+    
     //MARK: internal
     
-    
+    class func factoryItems(recipe:Recipe) -> [CookItemProtocol]
+    {
+        let itemTitle:CookItemTitle = CookItemTitle(recipe:recipe)
+        let itemIngredientsTitle:CookItemIngredientsTitle = CookItemIngredientsTitle()
+        let itemsIngredient:[CookItemProtocol] = Cook.factoryIngredientItems(recipe:recipe)
+        
+        var items:[CookItemProtocol] = [
+            itemTitle,
+            itemIngredientsTitle]
+        
+        items.append(contentsOf:itemsIngredient)
+        
+        return items
+    }
 }
--- Source/Model/Cook/Cook.swift
+++ Source/Model/Cook/Cook.swift
@@ -2,5 +2,29 @@ import Foundation
 
 final class Cook:Model<ArchCook>
 {
+    var items:[CookItemProtocol]
+    
     var homeItem:HomeItem?
+    {
+        didSet
+        {
+            guard
+                
+                let recipe:Recipe = self.homeItem?.recipe
+            
+            else
+            {
+                return
+            }
+            
+            self.items = Cook.factoryItems(recipe:recipe)
+        }
+    }
+    
+    required init()
+    {
+        self.items = []
+        
+        super.init()
+    }
 }
