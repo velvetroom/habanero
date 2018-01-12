a9534126f70692828a15f06a18be91a4a2f7d502
vauxhall <zero.griffin@gmail.com>
2018-01-12 15:33:11 -0600

    Refactor recipe ingredient quantity

--- Source/Model/Format/FormatIngredientTypeQuantity.swift
+++ Source/Model/Format/FormatIngredientTypeQuantity.swift
@@ -2,6 +2,22 @@ import Foundation
 
 struct FormatIngredientTypeQuantity:FormatIngredientTypeProtocol
 {
+    //MARK: private
+    
+    private static func factoryAmount(
+        quantity:Float,
+        settings:Settings) -> String?
+    {
+        let formatter:NumberFormatter = Format.factoryFormatter(
+            decimals:FormatIngredientTypeQuantity.Constants.maxDecimals)
+        formatter.positiveSuffix = String.localizedModel(key:"FormatIngredientTypeQuantity_suffix")
+        
+        let number:NSNumber = quantity as NSNumber
+        let amount:String? = formatter.string(from:number)
+        
+        return amount
+    }
+    
     //MARK: internal
     
     static func factoryAmount(
@@ -17,12 +33,29 @@ struct FormatIngredientTypeQuantity:FormatIngredientTypeProtocol
             return nil
         }
         
-        let formatter:NumberFormatter = Format.factoryFormatter(
-            decimals:FormatIngredientTypeQuantity.Constants.maxDecimals)
-        formatter.positiveSuffix = String.localizedModel(key:"FormatIngredientTypeQuantity_suffix")
+        let amount:String? = FormatIngredientTypeQuantity.factoryAmount(
+            quantity:buildIngredient.quantity,
+            settings:settings)
         
-        let number:NSNumber = buildIngredient.quantity as NSNumber
-        let amount:String? = formatter.string(from:number)
+        return amount
+    }
+    
+    static func factoryAmount(
+        recipeIngredient:RecipeIngredient,
+        settings:Settings) -> String?
+    {
+        guard
+            
+            let recipeIngredient:RecipeIngredientQuantity = recipeIngredient as? RecipeIngredientQuantity
+            
+        else
+        {
+            return nil
+        }
+        
+        let amount:String? = FormatIngredientTypeQuantity.factoryAmount(
+            quantity:recipeIngredient.quantity,
+            settings:settings)
         
         return amount
     }
