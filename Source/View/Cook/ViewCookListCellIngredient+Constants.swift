79eef69f588b8cc481dff6d06855f0863263c1cb
vauxhall <zero.griffin@gmail.com>
2018-01-13 04:33:34 -0600

    Add label text to ingredient cell

--- Source/View/Cook/ViewCookListCellIngredient+Constants.swift
+++ Source/View/Cook/ViewCookListCellIngredient+Constants.swift
@@ -5,5 +5,8 @@ extension ViewCookListCellIngredient
     struct Constants
     {
         static let squareSize:CGFloat = 14
+        static let textLeft:CGFloat = 5
+        static let nameFontSize:CGFloat = 16
+        static let amountFontSize:CGFloat = 14
     }
 }
--- Source/View/Cook/ViewCookListCellIngredient.swift
+++ Source/View/Cook/ViewCookListCellIngredient.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class ViewCookListCellIngredient:ViewCookListCell
 {
+    private weak var labelText:UILabel!
+    
     override func factoryViews()
     {
         super.factoryViews()
@@ -13,7 +15,15 @@ final class ViewCookListCellIngredient:ViewCookListCell
         square.contentMode = UIViewContentMode.center
         square.image = #imageLiteral(resourceName: "assetGenericSquare")
         
+        let labelText:UILabel = UILabel()
+        labelText.translatesAutoresizingMaskIntoConstraints = false
+        labelText.isUserInteractionEnabled = false
+        labelText.backgroundColor = UIColor.clear
+        labelText.numberOfLines = 0
+        self.labelText = labelText
+        
         self.addSubview(square)
+        self.addSubview(labelText)
         
         NSLayoutConstraint.topToTop(
             view:square,
@@ -26,5 +36,20 @@ final class ViewCookListCellIngredient:ViewCookListCell
         NSLayoutConstraint.size(
             view:square,
             constant:ViewCookListCellIngredient.Constants.squareSize)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelText,
+            toView:self,
+            constant:ViewCookList.Constants.marginHorizontal)
+        NSLayoutConstraint.heightGreaterOrEqual(
+            view:labelText)
+        NSLayoutConstraint.leftToRight(
+            view:labelText,
+            toView:square,
+            constant:ViewCookListCellIngredient.Constants.textLeft)
+        NSLayoutConstraint.rightToRight(
+            view:labelText,
+            toView:self,
+            constant:-ViewCookList.Constants.marginHorizontal)
     }
 }
