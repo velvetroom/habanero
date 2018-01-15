fab5384a10f5025cb9ebcf0117c3aa4f1093867e
vauxhall <zero.griffin@gmail.com>
2018-01-14 18:06:53 -0600

    Add button to play view

--- Source/View/Cook/ViewCookPlay.swift
+++ Source/View/Cook/ViewCookPlay.swift
@@ -2,5 +2,30 @@ import UIKit
 
 final class ViewCookPlay:View<ArchCook>
 {
+    private weak var button:UIButton!
     
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(
+            #imageLiteral(resourceName: "assetCookPlayReleased"),
+            for:UIControlState.normal)
+        button.setImage(
+            #imageLiteral(resourceName: "assetCookPlayHighlighted"),
+            for:UIControlState.highlighted)
+        button.setImage(
+            #imageLiteral(resourceName: "assetCookPlayHighlighted"),
+            for:UIControlState.selected)
+        button.imageView!.clipsToBounds = true
+        button.imageView!.contentMode = UIViewContentMode.center
+        
+        self.addSubview(button)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
+    }
 }
