42cbb7495a4a6280e37587b4dced8c4d380a745f
vauxhall <zero.griffin@gmail.com>
2018-01-15 23:36:25 -0600

    Create view cook step bar

--- Source/View/CookStep/ViewCookStep.swift
+++ Source/View/CookStep/ViewCookStep.swift
@@ -5,6 +5,5 @@ final class ViewCookStep:ViewMain<ArchCookStep>
     override func factoryViews()
     {
         super.factoryViews()
-        self.backgroundColor = UIColor.colourHabanero
     }
 }
