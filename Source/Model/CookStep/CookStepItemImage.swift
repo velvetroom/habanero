35996aca4585075e6a4f7796359621c81be556ce
vauxhall <zero.griffin@gmail.com>
2018-01-16 17:15:16 -0600

    Implement item protocol

--- Source/Model/CookStep/CookStepItemImage.swift
+++ Source/Model/CookStep/CookStepItemImage.swift
@@ -2,5 +2,12 @@ import UIKit
 
 struct CookStepItemImage:CookStepItemProtocol
 {
+    let reusableIdentifier:String = ViewCookStepListCellImage.reusableIdentifier
+    let text:String
+    var image:UIImage?
     
+    init(text:String)
+    {
+        self.text = text
+    }
 }
--- Source/Model/CookStep/CookStepItemText.swift
+++ Source/Model/CookStep/CookStepItemText.swift
@@ -2,5 +2,11 @@ import Foundation
 
 struct CookStepItemText:CookStepItemProtocol
 {
+    let reusableIdentifier:String = ViewCookStepListCellText.reusableIdentifier
+    let text:String
     
+    init(text:String)
+    {
+        self.text = text
+    }
 }
