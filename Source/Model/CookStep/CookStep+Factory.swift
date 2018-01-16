b3bd314767008218f38d3141703f2f970c12abb9
vauxhall <zero.griffin@gmail.com>
2018-01-16 17:50:06 -0600

    Factory step items

--- Source/Model/CookStep/CookStep+Factory.swift
+++ Source/Model/CookStep/CookStep+Factory.swift
@@ -2,7 +2,75 @@ import Foundation
 
 extension CookStep
 {
+    private static var typeRouter:[RecipeStepType:((RecipeStep) -> (CookStepItemProtocol?))]
+    {
+        get
+        {
+            let map:[RecipeStepType:((RecipeStep) -> (CookStepItemProtocol?))] = [
+                RecipeStepType.text : CookStep.factoryItemText,
+                RecipeStepType.image : CookStep.factoryItemImage]
+            
+            return map
+        }
+    }
+    
+    //MARK: private
+    
+    private class func factoryItemText(step:RecipeStep) -> CookStepItemProtocol?
+    {
+        guard
+        
+            let step:RecipeStepText = step as? RecipeStepText
+        
+        else
+        {
+            return nil
+        }
+        
+        let item:CookStepItemText = CookStepItemText(text:step.text)
+        
+        return item
+    }
+    
+    private class func factoryItemImage(step:RecipeStep) -> CookStepItemProtocol?
+    {
+        guard
+            
+            let step:RecipeStepImage = step as? RecipeStepImage
+            
+        else
+        {
+            return nil
+        }
+        
+        let item:CookStepItemImage = CookStepItemImage(
+            text:step.text,
+            imageIdentifier:step.identifier)
+        
+        return item
+    }
+    
     //MARK: internal
     
-    
+    class func factoryItems(item:HomeItem) -> [CookStepItemProtocol]
+    {
+        var items:[CookStepItemProtocol] = []
+        
+        for step:RecipeStep in item.steps
+        {
+            guard
+            
+                let router:((RecipeStep) -> (CookStepItemProtocol?)) = CookStep.typeRouter[step.stepType],
+                let item:CookStepItemProtocol = router(step)
+            
+            else
+            {
+                continue
+            }
+            
+            items.append(item)
+        }
+        
+        return items
+    }
 }
--- Source/Model/CookStep/CookStepItemImage.swift
+++ Source/Model/CookStep/CookStepItemImage.swift
@@ -4,10 +4,14 @@ struct CookStepItemImage:CookStepItemProtocol
 {
     let reusableIdentifier:String = ViewCookStepListCellImage.reusableIdentifier
     let text:String
+    let imageIdentifier:String
     var image:UIImage?
     
-    init(text:String)
+    init(
+        text:String,
+        imageIdentifier:String)
     {
         self.text = text
+        self.imageIdentifier = imageIdentifier
     }
 }
