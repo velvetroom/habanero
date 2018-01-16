43a30a6acbed8493fd094aa26bc22386074cc0e5
vauxhall <zero.griffin@gmail.com>
2018-01-16 00:55:14 -0600

    Layout button close

--- Source/View/CookStep/ViewCookStepBar.swift
+++ Source/View/CookStep/ViewCookStepBar.swift
@@ -2,5 +2,36 @@ import UIKit
 
 final class ViewCookStepBar:View<ArchCookStep>
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let buttonClose:UIButton = UIButton()
+        buttonClose.translatesAutoresizingMaskIntoConstraints = false
+        buttonClose.setImage(
+            #imageLiteral(resourceName: "assetGenericClose").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        buttonClose.setImage(
+            #imageLiteral(resourceName: "assetGenericClose").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        buttonClose.imageView!.tintColor = UIColor(white:0, alpha:0.2)
+        buttonClose.imageView!.clipsToBounds = true
+        buttonClose.imageView!.contentMode = UIViewContentMode.center
+        
+        self.addSubview(buttonClose)
+        
+        NSLayoutConstraint.topToBottom(
+            view:buttonClose,
+            toView:self,
+            constant:ViewGlobal.Constants.contentTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:buttonClose,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonClose,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonClose,
+            constant:ViewCookStepBar.Constants.closeWidth)
+    }
 }
