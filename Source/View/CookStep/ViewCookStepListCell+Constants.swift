29e8d3034cda4847fb48f4bc09eff984c5b80f53
vauxhall <zero.griffin@gmail.com>
2018-01-17 10:43:28 -0600

    Layout label text in cell

--- Source/View/CookStep/ViewCookStepListCell+Constants.swift
+++ Source/View/CookStep/ViewCookStepListCell+Constants.swift
@@ -5,5 +5,6 @@ extension ViewCookStepListCell
     struct Constants
     {
         static let textMarginHorizontal:CGFloat = 20
+        static let textFontSize:CGFloat = 20
     }
 }
--- Source/View/CookStep/ViewCookStepListCell.swift
+++ Source/View/CookStep/ViewCookStepListCell.swift
@@ -2,6 +2,33 @@ import UIKit
 
 class ViewCookStepListCell:ViewCollectionCell
 {
+    private(set) weak var labelText:UILabel!
+    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        self.isUserInteractionEnabled = false
+        
+        let labelText:UILabel = UILabel()
+        labelText.translatesAutoresizingMaskIntoConstraints = false
+        labelText.isUserInteractionEnabled = false
+        labelText.backgroundColor = UIColor.clear
+        labelText.numberOfLines = 0
+        labelText.font = UIFont.regular(size:ViewCookStepListCell.Constants.textFontSize)
+        labelText.textColor = UIColor.colourBackgroundDark
+        self.labelText = labelText
+        
+        self.addSubview(labelText)
+        
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelText,
+            toView:self,
+            margin:ViewCookStepListCell.Constants.textMarginHorizontal)
+        NSLayoutConstraint.bottomToBottom(
+            view:labelText,
+            toView:self)
+    }
+    
     //MARK: internal
     
     func config(
