36418c4d6f6d57a1a7f39e6a84c7cb03330067fe
vauxhall <zero.griffin@gmail.com>
2018-01-21 01:56:24 +0100

    Update item menu favourites

--- Source/Model/Menu/MenuItemFavourites.swift
+++ Source/Model/Menu/MenuItemFavourites.swift
@@ -2,7 +2,7 @@ import UIKit
 
 struct MenuItemFavourites:MenuItemGenericProtocol
 {
-    typealias A = ArchHome
+    typealias A = ArchFavourites
     
     let order:Menu.Order = Menu.Order.favourite
     let icon:UIImage = #imageLiteral(resourceName: "assetMenuFavourites")
