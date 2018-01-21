0fa4d149c7592c08157060a5cb4187a9a9ac4745
vauxhall <zero.griffin@gmail.com>
2018-01-21 02:41:44 +0100

    Layout view bar

--- Source/View/Favourites/ViewFavourites.swift
+++ Source/View/Favourites/ViewFavourites.swift
@@ -2,5 +2,22 @@ import UIKit
 
 final class ViewFavourites:ViewMain<ArchFavourites>
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let viewBar:ViewFavouritesBar = ViewFavouritesBar(controller:self.controller)
+        
+        self.addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:ViewGlobal.Constants.barHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
