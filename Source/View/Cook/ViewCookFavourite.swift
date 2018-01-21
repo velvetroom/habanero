fd1c758a01a4955c1cfdc3f6a942045432c6ac6e
vauxhall <zero.griffin@gmail.com>
2018-01-22 00:27:18 +0100

    Add images to favourite view

--- Source/View/Cook/ViewCookFavourite.swift
+++ Source/View/Cook/ViewCookFavourite.swift
@@ -1,6 +1,27 @@
 import UIKit
 
-final class ViewCookFavourite:UIView
+final class ViewCookFavourite:UIButton
 {
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        self.clipsToBounds = true
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.setImage(
+            #imageLiteral(resourceName: "assetGenericFavouriteOff"),
+            for:UIControlState.normal)
+        self.setImage(
+            #imageLiteral(resourceName: "assetGenericFavouriteOn"),
+            for:UIControlState.highlighted)
+        self.setImage(
+            #imageLiteral(resourceName: "assetGenericFavouriteOn"),
+            for:UIControlState.selected)
+        self.imageView!.clipsToBounds = true
+        self.imageView!.contentMode = UIViewContentMode.center
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
