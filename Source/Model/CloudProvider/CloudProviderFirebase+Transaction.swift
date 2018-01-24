dcef956a39a49165b5dde24733b6d268a7123730
vauxhall <zero.griffin@gmail.com>
2018-01-24 15:28:14 +0100

    Update transaction on firebase

--- Source/Model/CloudProvider/CloudProviderFirebase+Transaction.swift
+++ Source/Model/CloudProvider/CloudProviderFirebase+Transaction.swift
@@ -5,38 +5,53 @@ extension CloudProviderFirebase
 {
     //MARK: internal
     
-    func transactionOnDocument(
-        identifier:String,
+    func transactionOnProperty(
+        property:String,
+        documentIdentifier:String,
         collectionPath:String,
         with deltaValue:Int,
         completion:@escaping((Error?) -> ()))
     {
+        let documentReference:DocumentReference = self.reference.collection(
+            collectionPath).document(documentIdentifier)
+        
         self.reference.runTransaction(
         { (transaction:Transaction, error:NSErrorPointer) -> Any? in
             
-            let sfDocument: DocumentSnapshot
-            do {
-                try sfDocument = transaction.getDocument(sfReference)
-            } catch let fetchError as NSError {
-                errorPointer?.pointee = fetchError
+            let snapshot:DocumentSnapshot
+            
+            do
+            {
+                try snapshot = transaction.getDocument(documentReference)
+            }
+            catch let fetchError as NSError
+            {
+                error?.pointee = fetchError
+                
                 return nil
             }
             
-            guard let oldPopulation = sfDocument.data()?["population"] as? Int else {
-                let error = NSError(
-                    domain: "AppErrorDomain",
-                    code: -1,
-                    userInfo: [
-                        NSLocalizedDescriptionKey: "Unable to retrieve population from snapshot \(sfDocument)"
-                    ]
-                )
-                errorPointer?.pointee = error
-                return nil
+            let currentValue:Int
+            
+            if let propertyValue:Int = snapshot.data()[property] as? Int
+            {
+                currentValue = propertyValue
+            }
+            else
+            {
+                currentValue = 0
             }
             
-            transaction.updateData(["population": oldPopulation + 1], forDocument: sfReference)
+            let newValue:Int = currentValue + deltaValue
+            
+            let updatedData:[AnyHashable:Any] = [
+                property:newValue]
+            
+            transaction.updateData(
+                updatedData,
+                forDocument:documentReference)
+            
             return nil
-            
         })
         { (object:Any?, error:Error?) in
             
