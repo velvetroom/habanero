47bb61f8465b1ff4a6f4feb41db6e23675cc86ba
vauxhall <zero.griffin@gmail.com>
2018-01-25 11:44:35 +0100

    Add label amount

--- Source/View/Cook/ViewCookFavourite.swift
+++ Source/View/Cook/ViewCookFavourite.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class ViewCookFavourite:UIButton
 {
+    private weak var labelAmount:UILabel!
+    
     init()
     {
         super.init(frame:CGRect.zero)
@@ -18,6 +20,15 @@ final class ViewCookFavourite:UIButton
             for:UIControlState.selected)
         self.imageView!.clipsToBounds = true
         self.imageView!.contentMode = UIViewContentMode.center
+        
+        let labelAmount:UILabel = UILabel()
+        labelAmount.translatesAutoresizingMaskIntoConstraints = false
+        labelAmount.isUserInteractionEnabled = false
+        labelAmount.backgroundColor = UIColor.clear
+        
+        self.labelAmount = labelAmount
+        
+        self.addSubview(labelAmount)
     }
     
     required init?(coder:NSCoder)
