ffe4cba81b90448eedd0f63ca1359412cb652408
vauxhall <zero.griffin@gmail.com>
2018-01-26 10:03:45 +0100

    Layout label amount

--- Source/View/Cook/ViewCookFavourite.swift
+++ Source/View/Cook/ViewCookFavourite.swift
@@ -3,6 +3,7 @@ import UIKit
 final class ViewCookFavourite:UIButton
 {
     private weak var labelAmount:UILabel!
+    private weak var layoutAmountLeft:NSLayoutConstraint!
     
     init()
     {
@@ -25,10 +26,21 @@ final class ViewCookFavourite:UIButton
         labelAmount.translatesAutoresizingMaskIntoConstraints = false
         labelAmount.isUserInteractionEnabled = false
         labelAmount.backgroundColor = UIColor.clear
-        
+        labelAmount.font = UIFont.medium(size:ViewCookFavourite.Constants.fontSize)
+        labelAmount.textColor = UIColor.colourBackgroundDark
         self.labelAmount = labelAmount
         
         self.addSubview(labelAmount)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelAmount,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:labelAmount,
+            toView:self)
+        layoutAmountLeft = NSLayoutConstraint.leftToLeft(
+            view:labelAmount,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
