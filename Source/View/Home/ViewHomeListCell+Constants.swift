636f6348f0c3d198fa14a65971dbadae0defe56d
vauxhall <zero.griffin@gmail.com>
2018-01-28 15:47:59 +0100

    Reduce blur height

--- Source/View/Home/ViewHomeListCell+Constants.swift
+++ Source/View/Home/ViewHomeListCell+Constants.swift
@@ -5,7 +5,7 @@ extension ViewHomeListCell
     struct Constants
     {
         static let animationDuration:TimeInterval = 0.15
-        static let blurHeight:CGFloat = 70
+        static let blurHeight:CGFloat = 45
         static let titleFontSize:CGFloat = 18
         static let animateScale:CGFloat = 0.9
         static let cornerRadius:CGFloat = 6
