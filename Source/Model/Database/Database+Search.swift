a5ddd676f82093f251777087089b7f3238c1bed7
vauxhall <zero.griffin@gmail.com>
2018-01-28 16:45:09 +0100

    Remove favourites search

--- Source/Model/Database/Database+Search.swift
+++ Source/Model/Database/Database+Search.swift
@@ -2,12 +2,4 @@ import Foundation
 
 extension Database
 {
-    //MARK: internal
-    
-    func searchFavourite(
-        recipe:Recipe,
-        completion:@escaping((RecipeFavourite?) -> ()))
-    {
-        
-    }
 }
