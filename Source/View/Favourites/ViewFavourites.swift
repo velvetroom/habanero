2d2828ab90ff65388267680d5c78f91c32602e20
vauxhall <zero.griffin@gmail.com>
2018-01-30 10:16:43 +0100

    Add spinner to view favourites

--- Source/View/Favourites/ViewFavourites.swift
+++ Source/View/Favourites/ViewFavourites.swift
@@ -2,13 +2,19 @@ import UIKit
 
 final class ViewFavourites:ViewMain<ArchFavourites>
 {
+    private weak var viewSpinner:ViewSpinner!
+    
     override func factoryViews()
     {
         super.factoryViews()
         
         let viewBar:ViewFavouritesBar = ViewFavouritesBar(controller:self.controller)
         
+        let viewSpinner:ViewSpinner = ViewSpinner()
+        self.viewSpinner = viewSpinner
+        
         self.addSubview(viewBar)
+        self.addSubview(viewSpinner)
         
         NSLayoutConstraint.topToTop(
             view:viewBar,
@@ -18,6 +24,10 @@ final class ViewFavourites:ViewMain<ArchFavourites>
             constant:ViewGlobal.Constants.barHeight)
         NSLayoutConstraint.equalsHorizontal(
             view:viewBar,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:viewSpinner,
             toView:self)
     }
 }
