e224fd032292ff17dfb164a2907238fbfc0d4ce8
vauxhall <zero.griffin@gmail.com>
2018-01-30 12:45:02 +0100

    Layout bar title

--- Source/View/Options/ViewOptions.swift
+++ Source/View/Options/ViewOptions.swift
@@ -1,6 +1,23 @@
-import Foundation
+import UIKit
 
 final class ViewOptions:ViewMain<ArchOptions>
 {
-    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let viewBar:ViewOptionsBar = ViewOptionsBar(controller:self.controller)
+        
+        self.addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:ViewGlobal.Constants.barHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
