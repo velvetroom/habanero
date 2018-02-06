b4ddeb6af93cd1ce8acd46ee993ce733bb80e94d
vauxhall <zero.griffin@gmail.com>
2018-02-06 09:51:01 +0100

    Add favourites map

--- Source/Controller/Home/ControllerHome+Favourites.swift
+++ Source/Controller/Home/ControllerHome+Favourites.swift
@@ -2,9 +2,42 @@ import Foundation
 
 extension ControllerHome
 {
+    private static var favouriteMap:[CookItemFavouriteType:((Recipe) -> ())]
+    {
+        get
+        {
+            let favouriteMap:[CookItemFavouriteType:((Recipe) -> ())] = [
+                CookItemFavouriteType.on : ControllerHome.favouriteSetOff,
+                CookItemFavouriteType.off : ControllerHome.favouriteSetOn]
+            
+            return favouriteMap
+        }
+    }
+    
     //MARK: private
+    
+    private static func favouriteSetOff(recipe:Recipe)
+    {
+        
+    }
+    
+    private static func favouriteSetOn(recipe:Recipe)
+    {
+        
+    }
     
     //MARK: internal
     
-    
+    func favouriteItem(
+        cookItem:CookItem,
+        completion:@escaping(() -> ()))
+    {
+        let database:Database = self.model.database
+        
+        cookItem.favourite.selected(
+            cookItem,
+            self.model.cloud,
+            database,
+            completion)
+    }
 }
