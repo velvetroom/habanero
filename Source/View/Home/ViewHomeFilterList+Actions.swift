3203d842d5e9b18a691dc1b5c68e1658cfdf17b3
Zero <zero.griffin@gmail.com>
2018-02-08 08:25:34 +0100

    Add model at index for list

--- Source/View/Home/ViewHomeFilterList+Actions.swift
+++ Source/View/Home/ViewHomeFilterList+Actions.swift
@@ -4,5 +4,10 @@ extension ViewHomeFilterList
 {
     //MARK: internal
     
-    
+    func modelAtIndex(index:IndexPath) -> HomeFilterOptionsProtocol
+    {
+        let item:HomeFilterOptionsProtocol = self.controller.model.filterOptions[index.item]
+        
+        return item
+    }
 }
