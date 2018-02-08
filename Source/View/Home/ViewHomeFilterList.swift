fc7de0f76658483fddfffeb9eb7480cafe81b00a
Zero <zero.griffin@gmail.com>
2018-02-08 09:52:34 +0100

    Configure cell size

--- Source/View/Home/ViewHomeFilterList.swift
+++ Source/View/Home/ViewHomeFilterList.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class ViewHomeFilterList:ViewCollection<ArchHome, ViewHomeFilterListCell, ViewCollectionFlow>
 {
+    private weak var cellSize:CGSize?
+    
     override func factoryViews()
     {
         super.factoryViews()
@@ -9,7 +11,45 @@ final class ViewHomeFilterList:ViewCollection<ArchHome, ViewHomeFilterListCell, 
         
     }
     
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = self.controller.model.filterOptions.count
+        
+        return count
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let width:CGFloat = collectionView.bounds.width
+            let totalCells:CGFloat = CGFloat(collectionView.numberOfItems(inSection:0))
+            let widthPerCell:CGFloat = width / totalCells
+            let height:CGFloat = collectionView.bounds.height
+            let cellSize:CGSize = CGSize(
+                width:widthPerCell,
+                height:height)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
         
     }
 }
