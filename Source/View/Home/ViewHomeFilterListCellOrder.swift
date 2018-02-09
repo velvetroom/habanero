ee134c00d10418750e5c213b568f7896c768cef5
Zero <zero.griffin@gmail.com>
2018-02-09 08:29:11 +0100

    Override config on cell order

--- Source/View/Home/ViewHomeFilterListCellOrder.swift
+++ Source/View/Home/ViewHomeFilterListCellOrder.swift
@@ -2,5 +2,21 @@ import UIKit
 
 final class ViewHomeFilterListCellOrder:ViewHomeFilterListCell
 {
-    
+    override func config(
+        controller:ControllerHome,
+        model:HomeFilterOptionsProtocol)
+    {
+        super.config(
+            controller:controller,
+            model:model)
+        
+        guard
+            
+            let model:HomeFilterOptionsOrder = model
+            
+        else
+        {
+            return
+        }
+    }
 }
