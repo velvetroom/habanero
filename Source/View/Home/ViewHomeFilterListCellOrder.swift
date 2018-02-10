5132777b097acca240c5605b6e1ef58ffb869e14
Zero <zero.griffin@gmail.com>
2018-02-10 23:28:35 +0100

    Add buttons for order

--- Source/View/Home/ViewHomeFilterListCellOrder.swift
+++ Source/View/Home/ViewHomeFilterListCellOrder.swift
@@ -2,6 +2,23 @@ import UIKit
 
 final class ViewHomeFilterListCellOrder:ViewHomeFilterListCell
 {
+    private weak var buttonDate:UIButton!
+    private weak var buttonFavourites:UIButton!
+    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let buttonDate:UIButton = UIButton()
+        self.buttonDate = buttonDate
+        
+        let buttonFavourites:UIButton = UIButton()
+        self.buttonFavourites = buttonFavourites
+        
+        self.addSubview(buttonDate)
+        self.addSubview(buttonFavourites)
+    }
+    
     override func config(controller:ControllerHome)
     {
         super.config(controller:controller)
