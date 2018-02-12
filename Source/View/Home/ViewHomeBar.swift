52e4d89bcf5b1e61438a2ef97165aba630789ade
Zero <zero.griffin@gmail.com>
2018-02-12 12:12:16 +0100

    Add button filters to home bar

--- Source/View/Home/ViewHomeBar.swift
+++ Source/View/Home/ViewHomeBar.swift
@@ -2,9 +2,27 @@ import UIKit
 
 final class ViewHomeBar:ViewBar<ArchHome>
 {
+    private weak var buttonFilters:UIButton!
+    
     override func factoryViews()
     {
         super.factoryViews()
         self.labelTitle.text = String.localizedView(key:"ViewHomeBar_labelTitle")
+        
+        let buttonFilters:UIButton = UIButton()
+        self.buttonFilters = buttonFilters
+        
+        self.addSubview(buttonFilters)
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonFilters,
+            toView:self,
+            constant:ViewGlobal.Constants.contentTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:buttonFilters,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:buttonFilters,
+            toView:self)
     }
 }
