f95ffe0310c4a7f49fa443ac9440b49c269a17f7
Zero <zero.griffin@gmail.com>
2018-02-13 09:23:05 +0100

    Update filter view layout

--- Source/View/Home/ViewHomeFilter.swift
+++ Source/View/Home/ViewHomeFilter.swift
@@ -5,6 +5,20 @@ final class ViewHomeFilter:View<ArchHome>
     override func factoryViews()
     {
         super.factoryViews()
-        self.backgroundColor = UIColor.white
+        self.backgroundColor = UIColor(white:0, alpha:0.1)
+        
+        let viewBorder:ViewBorder = ViewBorder(colour:UIColor(white:0, alpha:0.2))
+        
+        self.addSubview(viewBorder)
+        
+        NSLayoutConstraint.height(
+            view:viewBorder,
+            constant:ViewGlobal.Constants.borderWidth)
+        NSLayoutConstraint.bottomToBottom(
+            view:viewBorder,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBorder,
+            toView:self)
     }
 }
