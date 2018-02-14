17be73833ff78794e7cf4bcfb355f6d978e62361
Zero <zero.griffin@gmail.com>
2018-02-14 09:13:20 +0100

    Add keywords to filter struct

--- Source/Model/Home/HomeFilter.swift
+++ Source/Model/Home/HomeFilter.swift
@@ -4,4 +4,14 @@ struct HomeFilter
 {
     var show:HomeFilterShowProtocol
     var order:HomeFilterOrderProtocol
+    var keywords:String
+    
+    init(
+        show:HomeFilterShowProtocol,
+        order:HomeFilterOrderProtocol)
+    {
+        self.show = show
+        self.order = order
+        self.keywords = String()
+    }
 }
