662a7ca49ae5a236181c66f0fd71b87a8420f9f0
Zero <zero.griffin@gmail.com>
2018-02-15 09:19:22 +0100

    Add toggle filter to show protocol

--- Source/Model/Home/HomeFilterShowProtocol.swift
+++ Source/Model/Home/HomeFilterShowProtocol.swift
@@ -2,5 +2,7 @@ import Foundation
 
 protocol HomeFilterShowProtocol
 {
+    var toggleFilter:((ControllerHome) -> (ViewHomeFilterListCellShow) -> ()) { get }
+    
     func filter(items:[CookItem]) -> [CookItem]
 }
