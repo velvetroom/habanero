eb26adc13fefd12a00efdd9908d260557f5ac360
Zero <zero.griffin@gmail.com>
2018-02-16 08:55:38 +0100

    Add label title to cell

--- Source/View/Home/ViewHomeFilterListCell.swift
+++ Source/View/Home/ViewHomeFilterListCell.swift
@@ -3,6 +3,34 @@ import UIKit
 class ViewHomeFilterListCell:ViewCollectionCell
 {
     private(set) weak var controller:ControllerHome?
+    private(set) weak var labelTitle:UILabel!
+    
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.font = UIFont.regular(size:ViewHomeFilterListCell.Constants.fontSize)
+        labelTitle.textColor = UIColor.colourBackgroundDark
+        self.labelTitle = labelTitle
+        
+        self.addSubview(labelTitle)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:ViewHomeFilterListCell.Constants.titleHeight)
+        NSLayoutConstraint.leftToLeft(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.widthGreaterOrEqual(
+            view:labelTitle)
+    }
     
     //MARK: internal
     
