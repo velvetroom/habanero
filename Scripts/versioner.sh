b6b94942ef3438ab87b46cf4c8f8bbc2aba7a892
Zero <zero.griffin@gmail.com>
2018-02-16 11:49:09 +0100

    Update versioner

--- Scripts/versioner.sh
+++ Scripts/versioner.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sleep 1
 
-build=$(git rev-list HEAD --count)
+build=$(($(git rev-list HEAD --count) + 1))
 version=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 if [[ $version == *"."* ]]; then
@@ -22,4 +21,5 @@ fi
 
 new_version="$version_major.$version_minor.$build"
 
-echo $new_version
+echo "Version: $new_version"
+echo "Build: $build"
