d5665604d05e89782f713840c58f36e54d7f252a
Zero <zero.griffin@gmail.com>
2018-02-19 08:54:48 +0100

    Add list and bar to view calendar

--- Source/View/Calendar/ViewCalendar.swift
+++ Source/View/Calendar/ViewCalendar.swift
@@ -2,8 +2,20 @@ import UIKit
 
 final class ViewCalendar:ViewMain<ArchCalendar>
 {
+    private(set) weak var viewBar:ViewCalendarBar!
+    private(set) weak var viewList:ViewCalendarList!
+    
     override func factoryViews()
     {
         super.factoryViews()
+        
+        let viewBar:ViewCalendarBar = ViewCalendarBar(controller:self.controller)
+        self.viewBar = viewBar
+        
+        let viewList:ViewCalendarList = ViewCalendarList(controller:self.controller)
+        self.viewList = viewList
+        
+        self.addSubview(viewList)
+        self.addSubview(viewBar)
     }
 }
