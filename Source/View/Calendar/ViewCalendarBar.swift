d2fae05fa095306cb3da32a810372178b91aca08
Zero <zero.griffin@gmail.com>
2018-02-19 08:33:13 +0100

    Add label title

--- Source/View/Calendar/ViewCalendarBar.swift
+++ Source/View/Calendar/ViewCalendarBar.swift
@@ -2,5 +2,18 @@ import UIKit
 
 final class ViewCalendarBar:View<ArchCalendar>
 {
+    private(set) weak var labelTitle:UILabel!
     
+    override func factoryViews()
+    {
+        super.factoryViews()
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.isUserInteractionEnabled = false
+        self.labelTitle = labelTitle
+        
+        self.addSubview(labelTitle)
+    }
 }
